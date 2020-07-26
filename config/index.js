e3580c3b557637949d99308421d944f24ef5b25b
b2nil <kentus.ho@gmail.com>
2020-07-26 12:14:20 +0800

    enable __VUE_PROD_DEVTOOLS__

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ const config = {
   plugins: [],
   defineConstants: {
     __VUE_OPTIONS_API__: true,
-    __VUE_PROD_DEVTOOLS__: false
+    __VUE_PROD_DEVTOOLS__: true
   },
   alias: {
     '@/components': path.resolve(__dirname, '..', 'src/components'),
