789f0d9bc01a5ebc056bafb898734ee47d072201
b2nil <kentus.ho@gmail.com>
2020-07-26 09:14:19 +0800

    fix: toast always show

--- src/components/toast/toast.ts
+++ src/components/toast/toast.ts
@@ -1,4 +1,4 @@
-import { h, defineComponent, ref, Ref, nextTick, watch, computed } from "vue"
+import { h, defineComponent, nextTick, watch, computed, reactive, watchEffect } from "vue"
 import { Image, Text, View, CommonEventFunction, CommonEvent } from "@tarojs/components"
 import { AtToastProps } from "types/toast"
 import AtComponentWithDefaultProps from '@/components/mixins'
@@ -14,7 +14,11 @@ const AtToast = defineComponent({
         text: PropTypes.string,
         icon: PropTypes.string,
         image: PropTypes.string,
-        status: String as () => AtToastProps['status'],
+        status: { 
+            type: String as () => AtToastProps['status'], 
+            default: '' as AtToastProps['status'],
+            validator: (val: string) => ['', 'error', 'loading', 'success'].includes(val)
+        },
         duration: PropTypes.number.def(3000),
         hasMask: Boolean,
         onClick: func<CommonEventFunction>().def(() => () => {}),
@@ -22,44 +26,35 @@ const AtToast = defineComponent({
     },
 
     setup(props: AtToastProps, { slots }) {
-        const _timer: Ref<NodeJS.Timeout | null> = ref(null)
-        const _isOpened = ref(props.isOpened)
-        if (props.isOpened) {
-            makeTimer(props.duration || 0)
-        }
+        const state = reactive({
+            _timer: null as NodeJS.Timeout | null,
+            _isOpened: props.isOpened
+        })
 
-        watch(() => props, (nextProps) => {
-            if (!nextProps.isOpened) {
-                close()
-                return
-            }
+        watchEffect(() => handleChange())
 
-            if(!_isOpened.value) {
-                _isOpened.value = true
-            } else {
-                clearTimer()
-            }
-
-            makeTimer(nextProps.duration || 0)
+        watch(() => props.duration, (val) => {
+            handleChange()
         })
 
         function clearTimer() {
-            if(_timer.value) {
-                clearTimeout(_timer.value)
+            if(state._timer) {
+                clearTimeout(state._timer)
+                state._timer = null
             }
         }
 
         function makeTimer(duration: number) {
             if(duration === 0) return
 
-            _timer.value = setTimeout(() => {
+            state._timer = setTimeout(() => {
                 close()
             }, +duration);
         }
 
         function close() {
-            if(_isOpened.value) {
-                _isOpened.value = false
+            if(state._isOpened) {
+                state._isOpened = false
                 nextTick(handleClose)
                 clearTimer()
             }
@@ -74,57 +69,75 @@ const AtToast = defineComponent({
         function handleClick(e: CommonEvent) {
             if (props.status === 'loading') return
 
-            if (props.onClick) {
+            if (typeof props.onClick === 'function') {
                 return props.onClick(e)
             }
 
             close()
         }
 
-        return () => {
-            const realImg = props.image || statusImg[props.status!] || null
-            const isRenderIcon = !!(props.icon && !(props.image || statusImg[props.status!]))
+        function handleChange() {
+            if(!props.isOpened) {
+                close()
+                return
+            }
 
+            if(!state._isOpened) {
+                state._isOpened = true
+            } else {
+                clearTimer()
+            }
+
+            makeTimer(props.duration || 0)
+        }
+
+        return () => {
+            const realImg = computed(() => (props.image || statusImg[props.status!] || null))
+            const isRenderIcon = computed(() => !!(props.icon && !(props.image || statusImg[props.status!])))
             const rootClass = computed(() => classNames('at-toast', props.className))
 
             const bodyClass = computed(() => classNames('toast-body', {
                 'at-toast__body--custom-image': props.image,
-                'toast-body--text': !realImg && !props.icon,
+                'toast-body--text': !realImg.value && !props.icon,
                 [`at-toast__body--${props.status}`]: !!props.status
             }))
 
             const iconClass = computed(() => classNames('at-icon', {
                 [`at-icon-${props.icon}`]: props.icon
             }))
-
-            if (!_isOpened) return null
-
-            return (
-                h(View, { class: rootClass }, [
+            
+            return state._isOpened ? (
+                h(View, { class: rootClass.value }, [
+                    // mask layer
                     props.hasMask && h(View, { class: 'at-toast__overlay' }),
+                    // body
                     h(View, {
-                        class: bodyClass,
+                        class: bodyClass.value,
                         style: props.customStyle,
                         onTap: handleClick
                     }, [
+                        // body content
                         h(View, { class: 'toast-body-content' }, [
-                            realImg && h(View, { class: 'toast-body-content__img' }, [
+                            // use real image
+                            realImg.value && h(View, { class: 'toast-body-content__img' }, [
                                 h(Image, {
                                     class: 'toast-body-content__img-item',
-                                    src: realImg,
+                                    src: realImg.value,
                                     mode: 'scaleToFill'
                                 })
                             ]),
-                            isRenderIcon && h(View, { class: 'toast-body-content__icon' }, [
-                                h(Text, { class: iconClass })
+                            // use icon
+                            isRenderIcon.value && h(View, { class: 'toast-body-content__icon' }, [
+                                h(Text, { class: iconClass.value })
                             ]),
-                            props.text && h(View, { class: 'toast-body-content__icon' }, [
-                                h(Text, { class: iconClass }, props.text)
+                            // text
+                            props.text && h(View, { class: 'toast-body-content__info' }, [
+                                h(Text, null, props.text)
                             ])
                         ])
                     ])
                 ])
-            )
+            ): null
         }
     }
 })
