71119d2ad33e210feabd8555c65cb4fd8db70195
b2nil <kentus.ho@gmail.com>
2020-07-26 10:35:28 +0800

    fix: accordion animation issue

--- src/components/accordion/index.ts
+++ src/components/accordion/index.ts
@@ -1,55 +1,52 @@
-import { h, defineComponent, reactive, ref, watch, nextTick} from 'vue'
+import { h, defineComponent, reactive, ref, watch, computed } from 'vue'
 import classNames from 'classnames'
 import { Text, View } from '@tarojs/components'
+import AtComponentWithDefaultProps from "@/components/mixins";
 import { CommonEvent } from '@tarojs/components/types/common'
 import { AtAccordionProps, AtAccordionState } from "types/accordion";
-import { AtIconBaseProps } from "types/base";
 import { delayQuerySelector } from '../../utils/common'
 
 const AtAccordion = defineComponent({
+    extends: AtComponentWithDefaultProps,
+
     props: {
-        open: { 
-            type: Boolean, 
-            default: false 
+        open: Boolean,
+        title: {
+            type: String,
+            default: ''
         },
-        title: { 
-            type: String, 
-            default: '' 
+        icon: {
+            type: Object as () => AtAccordionProps['icon'],
+            default: () => ({ value: '' })
         },
-        icon: { 
-            type: Object as () => AtIconBaseProps, 
-            default: () => ({ value: '' }) 
+        hasBorder: {
+            type: Boolean,
+            default: true
         },
-        hasBorder: { 
-            type: Boolean, 
-            default: true 
+        isAnimation: {
+            type: Boolean,
+            default: true
         },
-        isAnimation: { 
-            type: Boolean, 
-            default: true 
+        note: {
+            type: String,
+            default: ''
         },
-        note: { 
-            type: String, 
-            default: '' 
-        },
-        onClick: { 
-            type: Function as unknown as () => (open: boolean, event: CommonEvent) => void, 
-            default: () => () => {}
+        onClick: {
+            type: Function as unknown as () => AtAccordionProps['onClick'],
+            default: () => () => { }
         }
     },
 
     setup(props: AtAccordionProps, { slots }) {
         const isCompleted = ref(true)
         const startOpen = ref(false)
-        const state = reactive({ wrapperHeight: 0 } as AtAccordionState)
+        const state = reactive<AtAccordionState>({ wrapperHeight: 0 })
 
-        watch(() => props.open, (val, oldVal) => {
-            if (val !== oldVal) {
-                startOpen.value = !!val && !!props.isAnimation
-                toggleWithAnimation()
-            }
+        watch(() => props.open, (val) => {
+            startOpen.value = !!val.open && !!props.isAnimation
+            toggleWithAnimation()
         })
-       
+
         function handleClick(e: CommonEvent) {
             if (!isCompleted.value) return
 
@@ -60,68 +57,61 @@ const AtAccordion = defineComponent({
             if (!isCompleted.value || !props.isAnimation) return
 
             isCompleted.value = false
-            delayQuerySelector(this, '.at-accordion__body').then((rect) => {
+            delayQuerySelector(this, '.at-accordion__body', 0).then((rect) => {
                 const height = parseInt(rect[0].height.toString())
-                const startHeight = props.open ? height : 0
-                const endHeight = props.open ? 0 : height
+                const startHeight = props.open ? 0 : height
+                const endHeight = props.open ? height : 0
 
                 startOpen.value = false
                 state.wrapperHeight = startHeight
-                nextTick(() => {
-                    setTimeout(() => {
-                        state.wrapperHeight = endHeight
-                        nextTick(() => {
-                            setTimeout(() => {
-                                isCompleted.value = true
-                                delete state.wrapperHeight
-                            }, 100)
-                        })                                    
-                    }, 700)
-                })                
+
+                setTimeout(() => {
+                    state.wrapperHeight = endHeight
+                }, 100)
+
+                setTimeout(() => {
+                    isCompleted.value = true
+                }, 700)
             })
-        }        
+        }
 
         return () => {
-             const rootClass = classNames('at-accordion', props.className)
-             const prefixClass = (props.icon && props.icon.prefixClass) || 'at-icon'
-             const iconClass = classNames({
-                 [prefixClass]: true,
-                 [`${prefixClass}-${props.icon && props.icon.value}`]: props.icon && props.icon.value,
-                 'at-accordion__icon': true
-             })
-             const headerClass = classNames('at-accordion__header', {
+            const rootClass = computed(() => classNames('at-accordion', props.className))
+            const prefixClass = computed(() => ((props.icon && props.icon.prefixClass) || 'at-icon'))
+            const iconClass = computed(() => classNames({
+                [prefixClass.value]: true,
+                [`${prefixClass.value}-${props.icon && props.icon.value}`]: props.icon && props.icon.value,
+                'at-accordion__icon': true
+            }))
+            const headerClass = computed(() => classNames('at-accordion__header', {
                 'at-accordion__header--noborder': !props.hasBorder
-             })
-             const arrowClass = classNames('at-accordion__arrow', {
-                 'at-accordion__arrow--folded': !!props.open
-             })
-             const contentClass = classNames('at-accordion__content', {
-                 'at-accordion__content--inactive': (!props.open && isCompleted.value) || startOpen.value
-             })
-             const iconStyle = {
-                 color: (props.icon && props.icon.color) || '',
-                 fontSize: (props.icon && `${props.icon.size}px`) || ''
-             }
-             const contentStyle = {
-                 height: `${state.wrapperHeight}px`
-             }
+            }))
+            const arrowClass = computed(() => classNames('at-accordion__arrow', {
+                'at-accordion__arrow--folded': !!props.open
+            }))
+            const contentClass = computed(() => classNames('at-accordion__content', {
+                'at-accordion__content--inactive': (!props.open && isCompleted.value) || startOpen.value
+            }))
+            const iconStyle = computed(() => ({
+                color: (props.icon && props.icon.color) || '',
+                fontSize: (props.icon && `${props.icon.size}px`) || ''
+            }))
+            const contentStyle = computed(() => ({
+                height: isCompleted.value ? '' : `${state.wrapperHeight}px`
+            }))
 
-             if (isCompleted.value) {
-                 contentStyle.height = ''
-             }
-
-            return h(View, { class: rootClass, style: props.customStyle }, [
-                h(View, { class: headerClass, onTap: handleClick}, [
-                    props.icon && props.icon.value && h(Text, { class: iconClass, style: iconStyle }),
+            return h(View, { class: rootClass.value, style: props.customStyle }, [
+                h(View, { class: headerClass.value, onTap: handleClick }, [
+                    props.icon && props.icon.value && h(Text, { class: iconClass.value, style: iconStyle.value }),
                     h(View, { class: 'at-accordion__info' }, [
-                        h(View, { class: 'at-accordion__info__title' }, props.title), 
+                        h(View, { class: 'at-accordion__info__title' }, props.title),
                         h(View, { class: 'at-accordion__info__note' }, props.note)
                     ]),
-                    h(View, { class: arrowClass }, [
+                    h(View, { class: arrowClass.value }, [
                         h(Text, { class: classNames('at-icon', 'at-icon-chevron-down') })
                     ])
                 ]),
-                h(View, { class: contentClass, style: contentStyle }, [
+                h(View, { class: contentClass.value, style: contentStyle.value }, [
                     h(View, { class: 'at-accordion__body' }, slots.default && slots.default())
                 ])
             ])
