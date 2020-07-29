2edddda33798fef11669edafe74ac23ea1829090
b2nil <kentus.ho@gmail.com>
2020-07-29 17:34:16 +0800

    add component float-layout

---
+++ src/components/float-layout/index.ts
@@ -0,0 +1,122 @@
+import { h, defineComponent, computed, ref, nextTick, watch } from "vue"
+
+import classNames from 'classnames'
+import { handleTouchScroll } from "@/utils/common"
+
+import { ScrollView, Text, View } from '@tarojs/components'
+import { CommonEvent } from '@tarojs/components/types/common'
+import { AtFloatLayoutProps } from 'types/float-layout'
+
+import AtComponentWithDefaultProps from '../mixins'
+
+const AtFloatLayout = defineComponent({
+    mixins: [AtComponentWithDefaultProps],
+
+    props: {
+        // 参数
+        isOpened: {
+            type: Boolean,
+            default: false,
+            required: true
+        },
+        title: {
+            type: String as () => AtFloatLayoutProps['title'],
+            default: '',
+        },
+        scrollX: Boolean,
+        scrollY: {
+            type: Boolean,
+            default: true,
+        },
+        scrollTop: Number as () => AtFloatLayoutProps['scrollTop'],
+        scrollLeft: Number as () => AtFloatLayoutProps['scrollLeft'],
+        upperThreshold: Number as () => AtFloatLayoutProps['upperThreshold'],
+        lowerThreshold: Number as () => AtFloatLayoutProps['lowerThreshold'],
+        scrollWithAnimation: Boolean,
+        // 事件
+        onClose: Function as unknown as () => AtFloatLayoutProps['onClose'],
+        onScroll: Function as unknown as () => AtFloatLayoutProps['onScroll'],
+        onScrollToUpper: Function as unknown as () => AtFloatLayoutProps['onScrollToUpper'],
+        onScrollToLower: Function as unknown as () => AtFloatLayoutProps['onScrollToLower'],
+    },
+
+    setup(props: AtFloatLayoutProps, { slots }) {
+        const _isOpened = ref<boolean>(props.isOpened)
+
+        watch(() => props.isOpened, (val, oldVal) => {
+            if (val === oldVal) {
+                handleTouchScroll(val)
+            }
+
+            if (val !== _isOpened.value) {
+                _isOpened.value = val
+            }
+        })
+
+        function handleClose() {
+            if (typeof props.onClose === 'function') {
+                props.onClose(this.$event)
+            }
+        }
+
+        function close() {
+            _isOpened.value = false
+            nextTick(handleClose)
+        }
+
+        function handleTouchMove(e: CommonEvent) {
+            e.stopPropagation()
+        }
+
+        return () => {
+            const rootClass = computed(() => classNames(
+                'at-float-layout',
+                {
+                    'at-float-layout--active': _isOpened.value
+                },
+                props.className
+            ))
+
+            return (
+                h(View, {
+                    class: rootClass.value,
+                    onTouchMove: handleTouchMove
+                }, [
+                    // overlay
+                    h(View, {
+                        class: 'at-float-layout__overlay',
+                        onTap: close
+                    }),
+                    // container layout
+                    h(View, { class: 'at-float-layout__container layout' }, [
+                        // header
+                        props.title && (
+                            h(View, { class: 'layout-header' }, [
+                                h(Text, { class: 'layout-header__title' }, props.title),
+                                h(View, { class: 'layout-header__btn-close', onTap: close }),
+                            ])
+                        ),
+                        // body
+                        h(View, { class: 'layout-body' }, [
+                            h(ScrollView, {
+                                class: 'layout-body__content',
+                                scrollX: props.scrollX,
+                                scrollY: props.scrollY,
+                                scrollTop: props.scrollTop,
+                                scrollLeft: props.scrollLeft,
+                                upperThreshold: props.upperThreshold,
+                                lowerThreshold: props.lowerThreshold,
+                                scrollWithAnimation: props.scrollWithAnimation,
+                                onScroll: props.onScroll,
+                                onScrollToLower: props.onScrollToLower,
+                                onScrollToUpper: props.onScrollToUpper,
+                            }, slots.default && slots.default())
+                        ])
+                    ])
+                ])
+            )
+        }
+    }
+})
+
+export default AtFloatLayout
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -16,7 +16,7 @@ const getEnvs = (): ENVS => {
         isWEAPP: env === ENV_TYPE.WEAPP,
         isALIPAY: env === ENV_TYPE.ALIPAY,
         isWEB: env === ENV_TYPE.WEB,
-    }    
+    }
 }
 
 function pxTransform(size: number): string {
@@ -62,29 +62,51 @@ function delayQuerySelector(_, selectorStr: string, delayTime = 500): Promise<[]
 
 const objectToString = (style: object | string): string => {
     if (style && typeof style === 'object') {
-      let styleStr = ''
-      Object.keys(style).forEach((key) => {
-        const lowerCaseKey = key.replace(/([A-Z])/g, '-$1').toLowerCase()
-        styleStr += `${lowerCaseKey}:${style[key]};`
-      })
-      return styleStr
+        let styleStr = ''
+        Object.keys(style).forEach((key) => {
+            const lowerCaseKey = key.replace(/([A-Z])/g, '-$1').toLowerCase()
+            styleStr += `${lowerCaseKey}:${style[key]};`
+        })
+        return styleStr
     } else if (style && typeof style === 'string') {
-      return style
+        return style
     }
     return ''
 }
 
 function mergeStyle(style1: object | string, style2: object | string): object | string {
     if (style1 && typeof style1 === 'object' && style2 && typeof style2 === 'object') {
-      return Object.assign({}, style1, style2)
+        return Object.assign({}, style1, style2)
     }
     return objectToString(style1) + objectToString(style2)
 }
 
+let scrollTop = 0
+function handleTouchScroll(flag: any): void {
+    if (ENV !== Taro.ENV_TYPE.WEB) {
+        return
+    }
+    if (flag) {
+        scrollTop = document.documentElement.scrollTop
 
-export { 
+        // 使body脱离文档流
+        document.body.classList.add('at-frozen')
+
+        // 把脱离文档流的body拉上去！否则页面会回到顶部！
+        document.body.style.top = `${-scrollTop}px`
+    } else {
+        document.body.style.top = ''
+        document.body.classList.remove('at-frozen')
+
+        document.documentElement.scrollTop = scrollTop
+    }
+}
+
+
+export {
     pxTransform,
     getEnvs,
     delayQuerySelector,
-    mergeStyle
+    mergeStyle,
+    handleTouchScroll
 }
