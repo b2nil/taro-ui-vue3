bf6d5b78622d0cdafa6a6373bb0476c2c34873c9
b2nil <kentus.ho@gmail.com>
2020-07-29 20:17:23 +0800

    add component countdown

---
+++ src/components/countdown/item/index.ts
@@ -0,0 +1,41 @@
+import { h, defineComponent } from "vue"
+
+import { Text, View } from '@tarojs/components'
+import { AtCountdownItemProps } from 'types/countdown'
+
+import AtComponentWithDefaultProps from '../../mixins'
+
+const AtCountdownItem = defineComponent({
+    mixins: [AtComponentWithDefaultProps],
+
+    props: {
+        // 参数
+        num: {
+            type: Number as () => AtCountdownItemProps['num'],
+            default: 0,
+            required: true
+        },
+        separator: {
+            type: String as () => AtCountdownItemProps['separator'],
+            default: ':',
+        },
+    },
+
+    setup(props: AtCountdownItemProps) {
+
+        function formatNum(num: number): string {
+            return num <= 9 ? `0${num}` : `${num}`
+        }
+
+        return () => (
+            h(View, { class: 'at-countdown__item' }, [
+                h(View, { class: 'at-countdown__item-box' }, [
+                    h(Text, { class: 'at-countdown__time' }, formatNum(props.num))
+                ]),
+                h(Text, { class: 'at-countdown__separator' }, props.separator)
+            ])
+        )
+    }
+})
+
+export default AtCountdownItem
