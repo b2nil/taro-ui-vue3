74d93b102f5fb906d44c3f66370fadc7180b7b7f
b2nil <kentsu.ho@gmail.com>
2020-08-08 06:52:33 +0800

    add indexes demo

---
+++ src/pages/navigation/indexes/index.vue
@@ -0,0 +1,101 @@
+<template>
+    <view
+        class="page"
+        style="height: 100vh;"
+    >
+        <view style="height: 100%;">
+            <at-indexes
+                :list="mockdata"
+                topKey="Top"
+                @click="handleClick"
+                @scroll-into-view="handleScrollIntoView"
+            >
+                <view class="custom-area">
+                    用户自定义内容
+                    <at-search-bar
+                        :value="searchbarValue"
+                        placeholder="跳转到指定Key"
+                        @hange="handleChange"
+                        @action-click="handleActionClick"
+                    />
+                </view>
+            </at-indexes>
+        </view>
+    </view>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue"
+import Taro from "@tarojs/taro"
+import AtIndexes from "@/components/indexes"
+import AtSearchBar from "@/components/search-bar"
+import mockData from './mock-data'
+
+export default defineComponent({
+
+    components: {
+        AtIndexes,
+        AtSearchBar,
+    },
+
+    setup() {
+        const searchbarValue = ref('')
+        const mockdata = ref(mockData)
+
+        let scrollIntoView = (key) => {
+            Taro.showToast({
+                title: `scrollIntoView: ${key}`,
+                icon: 'none'
+            })
+        }
+
+        function handleClick(item) {
+            Taro.showToast({
+                title: `onClick: ${item}`,
+                icon: 'none'
+            })
+        }
+
+        function handleActionClick() {
+            if (!searchbarValue.value) {
+                return
+            }
+
+            searchbarValue.value = ''
+
+            scrollIntoView && scrollIntoView(searchbarValue.value.toUpperCase())
+        }
+
+        function handleChange(value) {
+            searchbarValue.value = value
+        }
+
+        function handleScrollIntoView(fn) {
+            scrollIntoView = fn
+        }
+
+        return {
+            mockdata,
+            searchbarValue,
+            handleClick,
+            handleChange,
+            handleActionClick,
+            handleScrollIntoView
+        }
+    }
+})
+</script>
+
+<style lang="scss">
+.example-item {
+    color: #333;
+    font-size: 28px;
+}
+
+.custom-area {
+    padding: 80px 20px;
+    font-size: 28px;
+    text-align: center;
+    background: #fcfcfc;
+}
+</style>
