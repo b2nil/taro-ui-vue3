f322e27f1f3b633b63f23cb31b780a7f95c7fef3
b2nil <kentsu.ho@gmail.com>
2020-08-07 15:08:34 +0800

    rewrite using page, panel wrappers

--- src/pages/layout/card/index.config.ts
+++
@@ -1,3 +0,0 @@
-export default {
-    navigationBarTitleText: '卡片组件展示'
-}
--- src/pages/layout/card/index.vue
+++ src/pages/layout/card/index.vue
@@ -1,120 +1,116 @@
 <template>
-    <view class="page">
-        <!-- header -->
-        <docs-header title="Card 卡片" />
-        <!-- body -->
-        <view class="doc-body">
-            <!-- 基础卡片 -->
-            <view class="panel">
-                <view class="panel__title">基础卡片</view>
-                <view class="panel_content no-padding">
-                    <view class="example-item">
-                        <at-card
-                            title="这是标题"
-                            @click="handleClick"
-                        >这也是内容区 可以随意定义功能</at-card>
-                    </view>
-                </view>
-            </view>
-            <!-- 带缩略图的卡片 -->
-            <view class="panel">
-                <view class="panel__title">带缩略图的卡片</view>
-                <view class="panel_content no-padding">
-                    <view class="example-item">
-                        <at-card
-                            title="这是标题"
-                            thumb="http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png"
-                        >这也是内容区 可以随意定义功能</at-card>
-                    </view>
-                </view>
-            </view>
-            <!-- 带图标的卡片 -->
-            <view class="panel">
-                <view class="panel__title">带图标的卡片</view>
-                <view class="panel_content no-padding">
-                    <view class="example-item">
-                        <at-card
-                            title="这是标题"
-                            :icon="{ value: 'tags', color: '#77a1fd' }"
-                        >这也是内容区 可以随意定义功能</at-card>
-                    </view>
-                </view>
-            </view>
-            <!-- 自定义图标的卡片 -->
-            <view class="panel">
-                <view class="panel__title">自定义图标的卡片</view>
-                <view class="panel_content no-padding">
-                    <view class="example-item">
-                        <at-card
-                            title="这是标题"
-                            :renderIcon="renderIcon"
-                        >这也是内容区 可以随意定义功能</at-card>
-                    </view>
-                </view>
-            </view>
-            <!-- 带小信息的卡片 -->
-            <view class="panel">
-                <view class="panel__title">带小信息的卡片</view>
-                <view class="panel__content no-padding">
-                    <view class="example-item">
-                        <at-card
-                            note="小Tips"
-                            title="这是个标题"
-                            thumb="http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png"
-                        >这也是内容区 可以随意定义功能</at-card>
-                    </view>
-                </view>
-            </view>
-            <!-- 自定义额外信息样式的卡片 -->
-            <view class="panel">
-                <view class="panel__title">自定义额外信息样式的卡片</view>
-                <view class="panel__content no-padding">
-                    <view class="example-item">
-                        <at-card
-                            note="小Tips"
-                            extra="2019/01/23 22:33:33"
-                            :extraStyle="{
+    <page header-title="Card 卡片">
+        <!-- 基础卡片 -->
+        <panel
+            title="基础卡片"
+            no-padding
+        >
+            <example-item>
+                <at-card
+                    title="这是标题"
+                    @click="handleClick"
+                >这也是内容区 可以随意定义功能</at-card>
+            </example-item>
+        </panel>
+        <!-- 带缩略图的卡片 -->
+        <panel
+            title="带缩略图的卡片"
+            no-padding
+        >
+            <example-item>
+                <at-card
+                    title="这是标题"
+                    thumb="http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png"
+                >这也是内容区 可以随意定义功能</at-card>
+            </example-item>
+        </panel>
+        <!-- 带图标的卡片 -->
+        <panel
+            title="带图标的卡片"
+            no-padding
+        >
+            <example-item>
+                <at-card
+                    title="这是标题"
+                    :icon="{ value: 'tags', color: '#77a1fd' }"
+                >这也是内容区 可以随意定义功能</at-card>
+            </example-item>
+        </panel>
+        <!-- 自定义图标的卡片 -->
+        <panel
+            title="自定义图标的卡片"
+            no-padding
+        >
+            <example-item>
+                <at-card
+                    title="这是标题"
+                    :renderIcon="renderIcon"
+                >这也是内容区 可以随意定义功能</at-card>
+            </example-item>
+        </panel>
+        <!-- 带小信息的卡片 -->
+        <panel
+            title="带小信息的卡片"
+            no-padding
+        >
+            <example-item>
+                <at-card
+                    note="小Tips"
+                    title="这是个标题"
+                    thumb="http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png"
+                >这也是内容区 可以随意定义功能</at-card>
+            </example-item>
+        </panel>
+        <!-- 自定义额外信息样式的卡片 -->
+        <panel
+            title="自定义额外信息样式的卡片"
+            no-padding
+        >
+            <example-item>
+                <at-card
+                    note="小Tips"
+                    extra="2019/01/23 22:33:33"
+                    :extraStyle="{
                                 fontSize: '12px',
                                 maxWidth: '200px',
                                 color: '#6190e8',
                             }"
-                            title="这是个标题"
-                            thumb="http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png"
-                        >这也是内容区 可以随意定义功能</at-card>
-                    </view>
-                </view>
-            </view>
-            <!-- 通栏卡片  -->
-            <view class="panel">
-                <view class="panel__title">通栏卡片</view>
-                <view class="panel__content no-padding">
-                    <view class="example-item">
-                        <at-card
-                            isFull
-                            note="小Tips"
-                            extra="额外信息"
-                            title="这是个标题"
-                            thumb="http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png"
-                        >这也是内容区 可以随意定义功能 这也是内容区 可以随意定义功能 这也是内容区 可以随意定义功能</at-card>
-                    </view>
-                </view>
-            </view>
-        </view>
-    </view>
+                    title="这是个标题"
+                    thumb="http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png"
+                >这也是内容区 可以随意定义功能</at-card>
+            </example-item>
+        </panel>
+        <!-- 通栏卡片  -->
+        <panel
+            title="通栏卡片"
+            no-padding
+        >
+            <example-item>
+                <at-card
+                    is-full
+                    note="小Tips"
+                    extra="额外信息"
+                    title="这是个标题"
+                    thumb="http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png"
+                >这也是内容区 可以随意定义功能 这也是内容区 可以随意定义功能 这也是内容区 可以随意定义功能</at-card>
+            </example-item>
+        </panel>
+    </page>
 </template>
 
 <script lang='ts'>
 import { h, defineComponent } from "vue"
 import Taro from "@tarojs/taro"
-import DocsHeader from '../../components/doc-header'
+import { Page, Panel, ExampleItem } from '../../components/demo-page'
 import AtIcon from '@/components/icon'
 import AtCard from '@/components/card'
 
 export default defineComponent({
-    name: 'CardPage',
 
     components: {
-        DocsHeader,
+        Page,
+        Panel,
+        ExampleItem,
         AtCard,
         AtIcon
     },
