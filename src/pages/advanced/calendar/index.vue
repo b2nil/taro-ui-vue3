b1ba64d1f8288f8cf83d3c289d6f1e31b04f6505
b2nil <kentsu.ho@gmail.com>
2020-08-08 20:55:27 +0800

    refactor(calendar demo): rewrite template

--- src/pages/advanced/calendar/index.vue
+++ src/pages/advanced/calendar/index.vue
@@ -1,121 +1,100 @@
 <template>
-    <view class="page calendar-page">
-        <view class="doc-body">
-            <view class="panel__title">一般案例</view>
-            <view class="panel__content">
-                <at-calendar
-                    @month-change="handleMonthChange"
-                    @day-click="handleDayClick"
-                />
-            </view>
-        </view>
+    <page
+        class="calendar-page"
+        header-title="Calendar 日历"
+    >
+        <panel title="一般案例">
+            <at-calendar @month-change="handleMonthChange" />
+        </panel>
 
-        <view class="doc-body">
-            <view class="panel__title">跳转到制定日期</view>
-            <view class="panel__content">
-                <at-calendar :currentDate="now" />
-                <view class="body_controllers">
-                    <at-button
-                        size="small"
-                        @click="handleClick('now', '2018/01/01')"
-                    >跳转到 2018/01/01</at-button>
-                    <at-button
-                        size="small"
-                        @click="handleClick('now', '2018/06/18')"
-                    >跳转到 2018/6/18</at-button>
-                </view>
+        <panel title="跳转到制定日期">
+            <at-calendar :currentDate="now" />
+            <view class="body_controllers">
+                <at-button
+                    size="small"
+                    @click="handleClick('now', '2018/01/01')"
+                >跳转到 2018/01/01</at-button>
+                <at-button
+                    size="small"
+                    @click="handleClick('now', '2018/06/18')"
+                >跳转到 2018/6/18</at-button>
             </view>
-        </view>
+        </panel>
 
-        <view class="doc-body">
-            <view class="panel__title">指定最小日期和最大日期</view>
-            <view class="panel__content">
-                <at-calendar
-                    :minDate="minDate"
-                    :maxDate="maxDate"
-                />
-                <view class="body_controllers">
-                    <at-button
-                        size="small"
-                        @click="handleClick('minDate', '2018/01/01')"
-                    >设置最小值 2018/1/1</at-button>
-                    <at-button
-                        size="small"
-                        @click="handleClick('maxDate', '2019/12/31')"
-                    >设置最大值 2019/12/31</at-button>
-                </view>
+        <panel title="指定最小日期和最大日期">
+            <at-calendar
+                :minDate="minDate"
+                :maxDate="maxDate"
+            />
+            <view class="body_controllers">
+                <at-button
+                    size="small"
+                    @click="handleClick('minDate', '2018/01/01')"
+                >设置最小值 2018/1/1</at-button>
+                <at-button
+                    size="small"
+                    @click="handleClick('maxDate', '2019/12/31')"
+                >设置最大值 2019/12/31</at-button>
             </view>
-        </view>
+        </panel>
 
-        <view class="doc-body">
-            <view class="panel__title">标记时间</view>
-            <view class="panel__content">
-                <at-calendar :marks="mark" />
-                <view class="body_controllers">
-                    <at-button
-                        size="small"
-                        class="button"
-                        @click="handleClick('mark', [{ value: Date.now() }])"
-                    >标记当前时间</at-button>
-                </view>
+        <panel title="标记时间">
+            <at-calendar :marks="mark" />
+            <view class="body_controllers">
+                <at-button
+                    size="small"
+                    class="button"
+                    @click="handleClick('mark', [{ value: Date.now() }])"
+                >标记当前时间</at-button>
             </view>
-        </view>
+        </panel>
 
-        <view class="doc-body">
-            <view class="panel__title">范围选择</view>
-            <view class="panel__content">
-                <at-calendar
-                    @select-date="handleDateChange"
-                    :currentDate="multiCurrentDate"
-                    isMultiSelect
-                />
-                <view class="body_controllers">
-                    <at-button
-                        size="small"
-                        class="button"
-                        @click="handleClick('multiCurrentDate', { start: '2018/10/28', end: '2018/11/11' })"
-                    >设置选择区间为 2018/10/28 - 2018/11/11</at-button>
-                </view>
+        <panel title="范围选择">
+            <at-calendar
+                @select-date="handleDateChange"
+                :currentDate="multiCurrentDate"
+                isMultiSelect
+            />
+            <view class="body_controllers">
+                <at-button
+                    size="small"
+                    class="button"
+                    @click="handleClick('multiCurrentDate', { start: '2018/10/28', end: '2018/11/11' })"
+                >设置选择区间为 2018/10/28 - 2018/11/11</at-button>
             </view>
-        </view>
+        </panel>
 
-        <view class="doc-body">
-            <view class="panel__title">禁止滑动</view>
-            <view class="panel__content">
-                <at-calendar :is-swiper="false" />
-            </view>
-        </view>
+        <panel title="禁止滑动">
+            <at-calendar :is-swiper="false" />
+        </panel>
 
-        <view class="doc-body">
-            <view class="panel__title">垂直滑动</view>
-            <view class="panel__content">
-                <at-calendar
-                    is-vertical
-                    @select-date="handleDateChange"
-                />
-            </view>
-        </view>
+        <panel title="垂直滑动">
+            <at-calendar
+                is-vertical
+                @select-date="handleDateChange"
+            />
+        </panel>
 
-        <view class="doc-body">
-            <view class="panel__title">有效时间组</view>
-            <view class="panel__content">
-                <at-calendar :validDates="validDates" />
-            </view>
-        </view>
-    </view>
+        <panel title="有效时间组">
+            <at-calendar :validDates="validDates" />
+        </panel>
+    </page>
 </template>
 
 <script>
-import { ref, computed, reactive, toRefs } from 'vue'
+import { ref, computed, reactive, toRefs, onMounted, onBeforeMount } from 'vue'
 import Taro from '@tarojs/taro'
 
 import AtButton from '@/components/button'
 import AtCalendar from '@/components/calendar'
+import { Page, Panel } from '../../components/demo-page'
 
 export default {
     components: {
         AtButton,
         AtCalendar,
+        Page,
+        Panel
     },
     setup() {
         const state = reactive({
@@ -158,6 +137,14 @@ export default {
             })
         }
 
+        onBeforeMount(() => {
+            Taro.showToast({
+                title: '正在加载...',
+                icon: 'loading',
+                duration: 7000
+            })
+        })
+
         return {
             ...toRefs(state),
             handleClick,
@@ -167,6 +154,10 @@ export default {
             handleMonthChange,
         }
     },
+
+    onShow() {
+        Taro.hideToast()
+    }
 }
 </script>
 
