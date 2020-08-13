5c7c227d052023b6042d6cd3b8af42384aff44ba
b2nil <kentsu.ho@gmail.com>
2020-08-13 18:04:05 +0800

    fix(utils): export taro-core h5 components

--- src/utils/components.ts
+++ src/utils/components.ts
@@ -1,52 +1,24 @@
-import { getEnvs } from './common'
-import {
-    View as TaroView,
-    Icon as TaroIcon,
-    Progress as TaroProgress,
-    Text as TaroText,
-    Button as TaroButton,
-    Checkbox as TaroCheckBox,
-    CheckboxGroup as TaroCheckBoxGroup,
-    Form as TaroForm,
-    Input as TaroInput,
-    Label as TaroLabel,
-    OpenData as TaroOpenData,
-    Picker as TaroPicker,
-    PickerView as TaroPickerView,
-    PickerViewColumn as TaroPickerViewColumn,
-    Radio as TaroRadio,
-    RadioGroup as TaroRadioGroup,
-    Slider as TaroSlider,
-    Switch as TaroSwitch,
-    Textarea as TaroTextarea,
-    ScrollView as TaroScrollView,
-    Swiper as TaroSwiper,
-    SwiperItem as TaroSwiperItem,
-    Image as TaroImage
-} from "@tarojs/components";
 
-const isWeb = getEnvs().isWEB
-
-export const View = isWeb ? 'taro-view-core' : TaroView
-export const Icon = isWeb ? 'taro-icon-core' : TaroIcon
-export const Progress = isWeb ? 'taro-progress-core' : TaroProgress
-export const Text = isWeb ? 'taro-text-core' : TaroText
-export const Button = isWeb ? 'taro-button-core' : TaroButton
-export const Checkbox = isWeb ? 'taro-checkbox-core' : TaroCheckBox
-export const CheckboxGroup = isWeb ? 'taro-checkbox-group-core' : TaroCheckBoxGroup
-export const Form = isWeb ? 'taro-form-core' : TaroForm
-export const Input = isWeb ? 'taro-input-core' : TaroInput
-export const Label = isWeb ? 'taro-label-core' : TaroLabel
-export const OpenData = isWeb ? 'taro-open-data-core' : TaroOpenData
-export const Picker = isWeb ? 'taro-picker-core' : TaroPicker
-export const PickerView = isWeb ? 'taro-picker-view-core' : TaroPickerView
-export const PickerViewColumn = isWeb ? 'taro-picker-view-column-core' : TaroPickerViewColumn
-export const Radio = isWeb ? 'taro-radio-core' : TaroRadio
-export const RadioGroup = isWeb ? 'taro-radio-group-core' : TaroRadioGroup 
-export const Slider = isWeb ? 'taro-slider-core' : TaroSlider
-export const Switch = isWeb ? 'taro-switch-core' : TaroSwitch
-export const Textarea = isWeb ? 'taro-textarea-core' : TaroTextarea
-export const ScrollView = isWeb ? 'taro-scroll-view-core' : TaroScrollView
-export const Swiper = isWeb ? 'taro-swiper-core' : TaroSwiper
-export const SwiperItem = isWeb ? 'taro-swiper-item-core' : TaroSwiperItem
-export const Image = isWeb ? 'taro-image-core' : TaroImage
+export const View = 'taro-view-core'
+export const Icon = 'taro-icon-core'
+export const Progress = 'taro-progress-core'
+export const Text = 'taro-text-core'
+export const Button = 'taro-button-core'
+export const Checkbox = 'taro-checkbox-core'
+export const CheckboxGroup = 'taro-checkbox-group-core'
+export const Form = 'taro-form-core'
+export const Input = 'taro-input-core'
+export const Label = 'taro-label-core'
+export const OpenData = 'taro-open-data-core'
+export const Picker = 'taro-picker-core'
+export const PickerView = 'taro-picker-view-core'
+export const PickerViewColumn = 'taro-picker-view-column-core'
+export const Radio = 'taro-radio-core'
+export const RadioGroup = 'taro-radio-group-core'
+export const Slider = 'taro-slider-core'
+export const Switch = 'taro-switch-core'
+export const Textarea = 'taro-textarea-core'
+export const ScrollView = 'taro-scroll-view-core'
+export const Swiper = 'taro-swiper-core'
+export const SwiperItem = 'taro-swiper-item-core'
+export const Image = 'taro-image-core'
