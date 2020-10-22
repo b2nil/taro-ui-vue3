313ae277216e6f2aa0470d292c83519237f2ac06
Ken Ho <kentsu.ho@gmail.com>
2020-10-22 14:45:59 +0800

    fix(docs): switch branch name master to main

--- packages/taro-ui-vue3-docs/deploy.sh
+++ packages/taro-ui-vue3-docs/deploy.sh
@@ -18,4 +18,4 @@ git add -A
 git commit -m "Deploy"
 
 # push to gh-pages branch
-git push -f git@github.com:b2nil/taro-ui-vue3.git master:gh-pages
+git push -f git@github.com:b2nil/taro-ui-vue3.git main:gh-pages
