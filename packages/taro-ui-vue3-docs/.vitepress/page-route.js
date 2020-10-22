abb82c6efc410b7a0f92795dd930d418ab1ba883
Ken Ho <kentsu.ho@gmail.com>
2020-10-22 22:24:37 +0800

    fix(virtual-scroll): fix route

--- packages/taro-ui-vue3-docs/.vitepress/page-route.js
+++ packages/taro-ui-vue3-docs/.vitepress/page-route.js
@@ -47,6 +47,6 @@ export default {
   tabs: 'navigation/tabs',
   indexes: 'navigation/indexes',
   calendar: 'advanced/calendar',
-  calendar: 'advanced/virtual-scroll',
+  virtualscroll: 'advanced/virtual-scroll',
   form: 'form/form'
 }
