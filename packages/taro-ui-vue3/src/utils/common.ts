ef80a32eb725d8f0d849f87703405118e0414f13
Ken Ho <kentsu.ho@gmail.com>
2020-11-01 12:53:21 +0800

    chore: update utils

--- packages/taro-ui-vue3/src/utils/common.ts
+++ packages/taro-ui-vue3/src/utils/common.ts
@@ -1,263 +1,281 @@
+import { ref, Ref, isRef } from '../api'
 import Taro, { SelectorQuery } from '@tarojs/taro'
+
+export type RefTyped<T> = T | Ref<T>;
+export type RefElement = Element | Ref<Element | undefined>;
+
+export type WrapRef<T> = T extends Ref<any> ? T : Ref<T>;
+
+export function unwrap<T>(o: RefTyped<T>): T;
+export function unwrap<T>(o: RefTyped<T>): T {
+  return isRef(o) ? o.value : o;
+  // return unref(o) as T;
+}
+
+export function wrap(o: RefElement): Ref<Element>;
+export function wrap<T>(o: RefTyped<T>): Ref<T>;
+export function wrap(o: any): any {
+  return isRef(o) ? o : ref(o); // NOTE in v3 this is not necessary
+}
 
 const { getEnv, ENV_TYPE } = Taro
 const ENV = Taro.getEnv()
 
 type ENVS = {
-    isWEAPP: boolean,
-    isALIPAY: boolean,
-    isWEB: boolean,
+  isWEAPP: boolean,
+  isALIPAY: boolean,
+  isWEB: boolean,
 }
 
 const getEnvs = (): ENVS => {
-    const env = getEnv()
+  const env = getEnv()
 
-    return {
-        isWEAPP: env === ENV_TYPE.WEAPP,
-        isALIPAY: env === ENV_TYPE.ALIPAY,
-        isWEB: env === ENV_TYPE.WEB,
-    }
+  return {
+    isWEAPP: env === ENV_TYPE.WEAPP,
+    isALIPAY: env === ENV_TYPE.ALIPAY,
+    isWEB: env === ENV_TYPE.WEB,
+  }
 }
 
 function pxTransform(size: number): string {
-    if (!size) return ''
+  if (!size) return ''
 
-    const designWidth = 750
-    const deviceRatio = {
-        640: 2.34 / 2,
-        750: 1,
-        828: 1.81 / 2,
-    }
+  const designWidth = 750
+  const deviceRatio = {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2,
+  }
 
-    return `${size / deviceRatio[designWidth]}rpx`
+  return `${size / deviceRatio[designWidth]}rpx`
 }
 
 function delay(delayTime = 500): Promise<null> {
-    return new Promise((resolve) => {
-        if ([Taro.ENV_TYPE.WEB, Taro.ENV_TYPE.SWAN, Taro.ENV_TYPE.WEAPP].includes(ENV)) {
-            setTimeout(() => {
-                resolve()
-            }, delayTime)
-            return
-        }
+  return new Promise((resolve) => {
+    if ([Taro.ENV_TYPE.WEB, Taro.ENV_TYPE.SWAN, Taro.ENV_TYPE.WEAPP].includes(ENV)) {
+      setTimeout(() => {
         resolve()
-    })
+      }, delayTime)
+      return
+    }
+    resolve()
+  })
 }
 
 function delayQuerySelector(_, selectorStr: string, delayTime = 500): Promise<[]> {
-    false && console.log(_)
-    const selector: SelectorQuery = Taro.createSelectorQuery()
+  false && console.log(_)
+  const selector: SelectorQuery = Taro.createSelectorQuery()
 
-    return new Promise((resolve) => {
-        delay(delayTime).then(() => {
-            selector
-                .select(selectorStr)
-                .boundingClientRect()
-                .exec((res: []) => {
-                    resolve(res)
-                })
+  return new Promise((resolve) => {
+    delay(delayTime).then(() => {
+      selector
+        .select(selectorStr)
+        .boundingClientRect()
+        .exec((res: []) => {
+          resolve(res)
         })
     })
+  })
 }
 
 const objectToString = (style: object | string): string => {
-    if (style && typeof style === 'object') {
-        let styleStr = ''
-        Object.keys(style).forEach((key) => {
-            const lowerCaseKey = key.replace(/([A-Z])/g, '-$1').toLowerCase()
-            styleStr += `${lowerCaseKey}:${style[key]};`
-        })
-        return styleStr
-    } else if (style && typeof style === 'string') {
-        return style
-    }
-    return ''
+  if (style && typeof style === 'object') {
+    let styleStr = ''
+    Object.keys(style).forEach((key) => {
+      const lowerCaseKey = key.replace(/([A-Z])/g, '-$1').toLowerCase()
+      styleStr += `${lowerCaseKey}:${style[key]};`
+    })
+    return styleStr
+  } else if (style && typeof style === 'string') {
+    return style
+  }
+  return ''
 }
 
 function mergeStyle(style1: object | string, style2: object | string): object | string {
-    if (style1 && typeof style1 === 'object' && style2 && typeof style2 === 'object') {
-        return Object.assign({}, style1, style2)
-    }
-    return objectToString(style1) + objectToString(style2)
+  if (style1 && typeof style1 === 'object' && style2 && typeof style2 === 'object') {
+    return Object.assign({}, style1, style2)
+  }
+  return objectToString(style1) + objectToString(style2)
 }
 
 let scrollTop = 0
 function handleTouchScroll(flag: any): void {
-    if (ENV !== Taro.ENV_TYPE.WEB) {
-        return
-    }
-    if (flag) {
-        scrollTop = document.documentElement.scrollTop
+  if (ENV !== Taro.ENV_TYPE.WEB) {
+    return
+  }
+  if (flag) {
+    scrollTop = document.documentElement.scrollTop
 
-        // 使body脱离文档流
-        document.body.classList.add('at-frozen')
+    // 使body脱离文档流
+    document.body.classList.add('at-frozen')
 
-        // 把脱离文档流的body拉上去！否则页面会回到顶部！
-        document.body.style.top = `${-scrollTop}px`
-    } else {
-        document.body.style.top = ''
-        document.body.classList.remove('at-frozen')
+    // 把脱离文档流的body拉上去！否则页面会回到顶部！
+    document.body.style.top = `${-scrollTop}px`
+  } else {
+    document.body.style.top = ''
+    document.body.classList.remove('at-frozen')
 
-        document.documentElement.scrollTop = scrollTop
-    }
+    document.documentElement.scrollTop = scrollTop
+  }
 }
 
 function uuid(len = 8, radix = 16): string {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('')
-    const value: string[] = []
-    let i = 0
-    radix = radix || chars.length
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('')
+  const value: string[] = []
+  let i = 0
+  radix = radix || chars.length
 
-    if (len) {
-        // Compact form
-        for (i = 0; i < len; i++) value[i] = chars[0 | (Math.random() * radix)]
-    } else {
-        // rfc4122, version 4 form
-        let r
+  if (len) {
+    // Compact form
+    for (i = 0; i < len; i++) value[i] = chars[0 | (Math.random() * radix)]
+  } else {
+    // rfc4122, version 4 form
+    let r
 
-        // rfc4122 requires these characters
-        /* eslint-disable-next-line */
-        value[8] = value[13] = value[18] = value[23] = '-'
-        value[14] = '4'
+    // rfc4122 requires these characters
+    /* eslint-disable-next-line */
+    value[8] = value[13] = value[18] = value[23] = '-'
+    value[14] = '4'
 
-        // Fill in random data.  At i==19 set the high bits of clock sequence as
-        // per rfc4122, sec. 4.1.5
-        for (i = 0; i < 36; i++) {
-            if (!value[i]) {
-                r = 0 | (Math.random() * 16)
-                value[i] = chars[i === 19 ? (r & 0x3) | 0x8 : r]
-            }
-        }
+    // Fill in random data.  At i==19 set the high bits of clock sequence as
+    // per rfc4122, sec. 4.1.5
+    for (i = 0; i < 36; i++) {
+      if (!value[i]) {
+        r = 0 | (Math.random() * 16)
+        value[i] = chars[i === 19 ? (r & 0x3) | 0x8 : r]
+      }
     }
+  }
 
-    return value.join('')
+  return value.join('')
 }
 
 function isTest(): boolean {
-    return process.env.NODE_ENV === 'test'
+  return process.env.NODE_ENV === 'test'
 }
 
 interface EventDetail {
-    pageX: number
-    pageY: number
-    clientX: number
-    clientY: number
-    offsetX: number
-    offsetY: number
-    x: number
-    y: number
+  pageX: number
+  pageY: number
+  clientX: number
+  clientY: number
+  offsetX: number
+  offsetY: number
+  x: number
+  y: number
 }
 
 function getEventDetail(event: any): EventDetail {
-    let detail: EventDetail
-    switch (ENV) {
-        case Taro.ENV_TYPE.WEB:
-            detail = {
-                pageX: event.pageX,
-                pageY: event.pageY,
-                clientX: event.clientX,
-                clientY: event.clientY,
-                offsetX: event.offsetX,
-                offsetY: event.offsetY,
-                x: event.x,
-                y: event.y
-            }
-            break
+  let detail: EventDetail
+  switch (ENV) {
+    case Taro.ENV_TYPE.WEB:
+      detail = {
+        pageX: event.pageX,
+        pageY: event.pageY,
+        clientX: event.clientX,
+        clientY: event.clientY,
+        offsetX: event.offsetX,
+        offsetY: event.offsetY,
+        x: event.x,
+        y: event.y
+      }
+      break
 
-        case Taro.ENV_TYPE.WEAPP:
-            detail = {
-                pageX: event.touches[0].pageX,
-                pageY: event.touches[0].pageY,
-                clientX: event.touches[0].clientX,
-                clientY: event.touches[0].clientY,
-                offsetX: event.target.offsetLeft,
-                offsetY: event.target.offsetTop,
-                x: event.target.x,
-                y: event.target.y
-            }
-            break
+    case Taro.ENV_TYPE.WEAPP:
+      detail = {
+        pageX: event.touches[0].pageX,
+        pageY: event.touches[0].pageY,
+        clientX: event.touches[0].clientX,
+        clientY: event.touches[0].clientY,
+        offsetX: event.target.offsetLeft,
+        offsetY: event.target.offsetTop,
+        x: event.target.x,
+        y: event.target.y
+      }
+      break
 
-        case Taro.ENV_TYPE.ALIPAY:
-            detail = {
-                pageX: event.target.pageX,
-                pageY: event.target.pageY,
-                clientX: event.target.clientX,
-                clientY: event.target.clientY,
-                offsetX: event.target.offsetLeft,
-                offsetY: event.target.offsetTop,
-                x: event.target.x,
-                y: event.target.y
-            }
-            break
+    case Taro.ENV_TYPE.ALIPAY:
+      detail = {
+        pageX: event.target.pageX,
+        pageY: event.target.pageY,
+        clientX: event.target.clientX,
+        clientY: event.target.clientY,
+        offsetX: event.target.offsetLeft,
+        offsetY: event.target.offsetTop,
+        x: event.target.x,
+        y: event.target.y
+      }
+      break
 
-        case Taro.ENV_TYPE.SWAN:
-            detail = {
-                pageX: event.changedTouches[0].pageX,
-                pageY: event.changedTouches[0].pageY,
-                clientX: event.target.clientX,
-                clientY: event.target.clientY,
-                offsetX: event.target.offsetLeft,
-                offsetY: event.target.offsetTop,
-                x: event.detail.x,
-                y: event.detail.y
-            }
-            break
+    case Taro.ENV_TYPE.SWAN:
+      detail = {
+        pageX: event.changedTouches[0].pageX,
+        pageY: event.changedTouches[0].pageY,
+        clientX: event.target.clientX,
+        clientY: event.target.clientY,
+        offsetX: event.target.offsetLeft,
+        offsetY: event.target.offsetTop,
+        x: event.detail.x,
+        y: event.detail.y
+      }
+      break
 
-        default:
-            detail = {
-                pageX: 0,
-                pageY: 0,
-                clientX: 0,
-                clientY: 0,
-                offsetX: 0,
-                offsetY: 0,
-                x: 0,
-                y: 0
-            }
-            console.warn('getEventDetail暂未支持该环境')
-            break
-    }
-    return detail
+    default:
+      detail = {
+        pageX: 0,
+        pageY: 0,
+        clientX: 0,
+        clientY: 0,
+        offsetX: 0,
+        offsetY: 0,
+        x: 0,
+        y: 0
+      }
+      console.warn('getEventDetail暂未支持该环境')
+      break
+  }
+  return detail
 }
 
 function delayGetScrollOffset({ delayTime = 500 }): Promise<any[]> {
-    return new Promise(resolve => {
-        delay(delayTime).then(() => {
-            Taro.createSelectorQuery()
-                .selectViewport()
-                .scrollOffset()
-                .exec((res: any[]) => {
-                    resolve(res)
-                })
+  return new Promise(resolve => {
+    delay(delayTime).then(() => {
+      Taro.createSelectorQuery()
+        .selectViewport()
+        .scrollOffset()
+        .exec((res: any[]) => {
+          resolve(res)
         })
     })
+  })
 }
 
 function delayGetClientRect({ selectorStr, delayTime = 500 }): Promise<any[]> {
-    const selector: SelectorQuery = Taro.createSelectorQuery()
+  const selector: SelectorQuery = Taro.createSelectorQuery()
 
-    return new Promise(resolve => {
-        delay(delayTime).then(() => {
-            selector
-                .select(selectorStr)
-                .boundingClientRect()
-                .exec((res: any[]) => {
-                    resolve(res)
-                })
+  return new Promise(resolve => {
+    delay(delayTime).then(() => {
+      selector
+        .select(selectorStr)
+        .boundingClientRect()
+        .exec((res: any[]) => {
+          resolve(res)
         })
     })
+  })
 }
 
 
 export {
-    pxTransform,
-    getEnvs,
-    delayQuerySelector,
-    mergeStyle,
-    handleTouchScroll,
-    uuid,
-    isTest,
-    getEventDetail,
-    delayGetClientRect,
-    delayGetScrollOffset,
+  pxTransform,
+  getEnvs,
+  delayQuerySelector,
+  mergeStyle,
+  handleTouchScroll,
+  uuid,
+  isTest,
+  getEventDetail,
+  delayGetClientRect,
+  delayGetScrollOffset,
 }
