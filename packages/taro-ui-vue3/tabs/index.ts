df4f86c74afb4a71bd38033b9d4509a2ae40332a
Ken Ho <kentsu.ho@gmail.com>
2020-11-11 19:03:14 +0800

    refactor(tabs-pane): switch to independent package

--- packages/taro-ui-vue3/tabs/index.ts
+++ packages/taro-ui-vue3/tabs/index.ts
@@ -1,10 +1,13 @@
 import { App } from 'vue'
 import AtTabs from './src/index.vue'
+import AtTabsPane from './src/pane/index.vue'
 
 export default (app: App): void => {
   app.component(AtTabs.name, AtTabs)
+  app.component(AtTabsPane.name, AtTabsPane)
 }
 
 export {
-  AtTabs
+  AtTabs,
+  AtTabsPane
 }
---
+++ packages/taro-ui-vue3/tabs/src/pane/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <view
+    v-bind="$attrs"
+    :class="rootClasses"
+  >
+    <slot />
+  </view>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, PropType } from 'vue'
+import { AtTabsPaneProps } from "@taro-ui-vue3/types/tabs-pane"
+
+export default defineComponent({
+  name: "AtTabsPane",
+
+  props: {
+    tabDirection: {
+      type: String as PropType<AtTabsPaneProps['tabDirection']>,
+      default: 'horizontal',
+      required: true
+    },
+    index: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    current: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+  },
+
+  setup(props: AtTabsPaneProps) {
+
+    const rootClasses = computed(() => ({
+      'at-tabs-pane--active': props.index === props.current,
+      'at-tabs-pane--inactive': props.index !== props.current,
+      'at-tabs-pane--vertical': props.tabDirection === 'vertical',
+      'at-tabs-pane': true,
+    }))
+
+    return {
+      rootClasses
+    }
+  }
+})
+</script>
