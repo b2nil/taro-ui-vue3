882be69ca7cdb37f053e1ec31872caccd259485b
Ken Ho <kentsu.ho@gmail.com>
2020-11-12 13:37:22 +0800

    refactor(components): switch to independent package

--- packages/taro-ui-vue3/config/rollup.config.analyze.js
+++
@@ -1,6 +0,0 @@
-import RollupVisualizer from 'rollup-plugin-visualizer'
-import defaultConfig from './rollup.config'
-
-export default Object.assign({}, defaultConfig, {
-  plugins: [...defaultConfig.plugins, RollupVisualizer()]
-})
