19f6c48e8149343d2a667fc43620be05d2e6288b
Ken Ho <kentsu.ho@gmail.com>
2020-11-12 09:27:44 +0800

    fix(curtain): export AtCurtain as module

--- packages/taro-ui-vue3/curtain/index.ts
+++ packages/taro-ui-vue3/curtain/index.ts
@@ -1,5 +1,10 @@
 import { App } from 'vue'
 import AtCurtain from './src/index.vue'
+
 export default (app: App): void => {
   app.component(AtCurtain.name, AtCurtain)
+}
+
+export {
+  AtCurtain
 }
