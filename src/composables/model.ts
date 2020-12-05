3678afc562700d0916b1599d06b43485eacd7ae2
Ken Ho <kentsu.ho@gmail.com>
2020-12-06 00:55:24 +0800

    feat(composables): add useModelValue utility

---
+++ src/composables/model.ts
@@ -0,0 +1,8 @@
+import { computed } from 'vue'
+
+export function useModelValue(props, emit, name = "modelValue") {
+  return computed({
+    get: () => props[name],
+    set: (value) => emit(`update:${name}`, value)
+  })
+}
