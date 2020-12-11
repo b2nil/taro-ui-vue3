71c935027bbe0ff4b016f39271d026bc382fcbf2
Ken Ho <kentsu.ho@gmail.com>
2020-12-11 12:36:53 +0800

    fix(tests): update activityIndicator test cases

--- src/components/activity-indicator/__tests__/activity-indicator.spec.ts
+++ src/components/activity-indicator/__tests__/activity-indicator.spec.ts
@@ -13,32 +13,32 @@ const factory = (values = {}, slots = { default: [] }) => {
 }
 
 describe('ActivityIndicator Snap', () => {
-  it('render initial ActivityIndicator', () => {
+  it('should render initial ActivityIndicator', () => {
     const wrapper = factory()
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render ActivityIndicator -- props size', () => {
+  it('should render ActivityIndicator -- props size', () => {
     const wrapper = factory({ size: 32 })
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render ActivityIndicator -- props color', () => {
+  it('should render ActivityIndicator -- props color', () => {
     const wrapper = factory({ color: '#13CE66' })
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render ActivityIndicator -- props content', () => {
+  it('should render ActivityIndicator -- props content', () => {
     const wrapper = factory({ content: '加载中...' })
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render ActivityIndicator -- props mode', () => {
+  it('should render ActivityIndicator -- props mode', () => {
     const wrapper = factory({ mode: 'center' })
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render ActivityIndicator -- props isOpened', () => {
+  it('should render ActivityIndicator -- props isOpened', () => {
     const wrapper = factory({ isOpened: true })
     expect(wrapper.element).toMatchSnapshot()
   })
