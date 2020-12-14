038addfea4bfb131bfc3ea517b9dba980e639e4f
Ken Ho <kentsu.ho@gmail.com>
2020-12-14 18:54:10 +0800

    fix(tests): update tab-bar test cases

--- src/components/tab-bar/__tests__/tab-bar.spec.ts
+++ src/components/tab-bar/__tests__/tab-bar.spec.ts
@@ -1,27 +1,25 @@
-import { mount } from '@vue/test-utils'
+import { mountFactory, Slots } from '@/tests/helper'
 import AtTabBar from '../index'
 
-const factory = (values = {}, slots = { default: [''] }) => {
-  return mount(AtTabBar as any, {
-    slots,
-    props: { ...values },
-  })
+const factory = (values = {}, slots: Slots = { default: [''] }) => {
+  return mountFactory(AtTabBar, {}, values, slots)
 }
 
+const tabList = [
+  { title: '待办事项', iconType: 'bullet-list', text: 'new' },
+  { title: '拍照', iconType: 'camera' },
+  { title: '文件夹', iconType: 'folder', text: '100', max: '99' },
+  {
+    title: '领取中心',
+    image:
+      'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png',
+    selectedImage:
+      'https://img14.360buyimg.com/jdphoto/s72x72_jfs/t17251/336/1311038817/3177/72595a07/5ac44618Na1db7b09.png',
+    text: 'new',
+  },
+]
+
 describe('AtTabBar Snap', () => {
-  const tabList = [
-    { title: '待办事项', iconType: 'bullet-list', text: 'new' },
-    { title: '拍照', iconType: 'camera' },
-    { title: '文件夹', iconType: 'folder', text: '100', max: '99' },
-    {
-      title: '领取中心',
-      image:
-        'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png',
-      selectedImage:
-        'https://img14.360buyimg.com/jdphoto/s72x72_jfs/t17251/336/1311038817/3177/72595a07/5ac44618Na1db7b09.png',
-      text: 'new',
-    },
-  ]
 
   it('should render initial AtTabBar', () => {
     const wrapper = factory()
@@ -54,7 +52,7 @@ describe('AtTabBar Snap', () => {
   })
 
   it('should render AtTabBar -- props fontSize', () => {
-    const wrapper = factory({ fontSize: '26', tabList: tabList })
+    const wrapper = factory({ fontSize: 26, tabList: tabList })
     expect(wrapper.element).toMatchSnapshot()
   })
 
@@ -70,5 +68,19 @@ describe('AtTabBar Snap', () => {
   it('should render AtTabBar -- props scroll', () => {
     const wrapper = factory({ scroll: true, tabList: tabList })
     expect(wrapper.element).toMatchSnapshot()
+  })
+})
+
+describe('AtTabBar Events', () => {
+  it('should trigger onClick event, and prop current should be equal to index of the item clicked', async () => {
+    const onClick = jest.fn()
+    const wrapper = factory({ tabList: tabList, onClick: onClick })
+
+    wrapper.find('.at-tab-bar__item:nth-child(3)').trigger('tap')
+    expect(onClick).toBeCalled()
+
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.current).toBe(2)
+    })
   })
 })
