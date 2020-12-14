b66a6bf09e4ba55d5826bed23ecf6c04b899945a
Ken Ho <kentsu.ho@gmail.com>
2020-12-14 10:16:11 +0800

    fix(tests): update search-bar test cases

--- src/components/search-bar/__tests__/search-bar.spec.ts
+++ src/components/search-bar/__tests__/search-bar.spec.ts
@@ -1,84 +1,84 @@
-import { mount } from '@vue/test-utils'
+import { mountFactory } from '@/tests/helper'
 import AtSearchBar from '../index'
 
+const onChange = jest.fn()
 const factory = (values = {}, slots = { default: [] }) => {
-  return mount(AtSearchBar as any, {
-    slots,
-    props: { ...values },
-  })
+  if (!values['v-model:value']) {
+    values = Object.assign(values, { onChange: onChange })
+  }
+  return mountFactory(AtSearchBar, {}, values, slots)
 }
 
 describe('AtSearchBar Snap', () => {
-  it('render initial AtSearchBar', () => {
+  it('should render initial AtSearchBar', () => {
     const wrapper = factory()
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render AtSearchBar -- props style', () => {
+  it('should render AtSearchBar -- props style', () => {
     const wrapper = factory({ style: 'color:red;' })
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render AtSearchBar -- props class', () => {
+  it('should render AtSearchBar -- props class', () => {
     const wrapper = factory({ class: 'test' })
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render AtSearchBar -- props value', () => {
+  it('should render AtSearchBar -- props value', () => {
     const wrapper = factory({ value: 'value' })
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render AtSearchBar -- props placeholder', () => {
+  it('should render AtSearchBar -- props placeholder', () => {
     const wrapper = factory({ placeholder: 'placeholder' })
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render AtSearchBar -- props maxLength', () => {
+  it('should render AtSearchBar -- props maxLength', () => {
     const wrapper = factory({ maxLength: 120 })
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render AtSearchBar -- props fixed', () => {
+  it('should render AtSearchBar -- props fixed', () => {
     const wrapper = factory({ fixed: true })
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render AtSearchBar -- props fucos', () => {
+  it('should render AtSearchBar -- props focus', () => {
     const wrapper = factory({ fucos: true })
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render AtSearchBar -- props disabled', () => {
+  it('should render AtSearchBar -- props disabled', () => {
     const wrapper = factory({ disabled: true })
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render AtSearchBar -- props showActionButton', () => {
+  it('should render AtSearchBar -- props showActionButton', () => {
     const wrapper = factory({ showActionButton: true })
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render AtSearchBar -- props actionName', () => {
+  it('should render AtSearchBar -- props actionName', () => {
     const wrapper = factory({ actionName: 'actionName' })
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('render AtSearchBar -- props inputType number', () => {
+  it('should render AtSearchBar -- props inputType number', () => {
     const wrapper = factory({ inputType: 'number' })
     expect(wrapper.element).toMatchSnapshot()
   })
 })
 
-describe('AtSearchBar Event', () => {
+describe('AtSearchBar Events', () => {
   it('AtSearchBar onChange', () => {
-    const onChange = jest.fn()
-    const wrapper = factory({ onChange: onChange })
-    wrapper.find('.at-search-bar .at-search-bar__input').trigger('input')
+    const wrapper = factory({ value: 'props.value' })
+    wrapper.find('.at-search-bar .at-search-bar__input').trigger('input', { detail: { value: 'value' } })
     expect(onChange).toBeCalled()
   })
 
-  it('AtSearchBar onChange', () => {
+  it('AtSearchBar onFocus', () => {
     const onFocus = jest.fn()
     const wrapper = factory({ onFocus: onFocus })
     wrapper.find('.at-search-bar .at-search-bar__input').trigger('focus')
@@ -104,5 +104,21 @@ describe('AtSearchBar Event', () => {
     const wrapper = factory({ onActionClick: onActionClick })
     wrapper.find('.at-search-bar .at-search-bar__action').trigger('tap')
     expect(onActionClick).toBeCalled()
+  })
+
+  it('AtSearchBar onClear', async () => {
+    const onClear = jest.fn()
+    const wrapper = factory({ onClear: onClear })
+    const clearElement = wrapper.find('.at-search-bar .at-search-bar__clear')
+    expect(clearElement.exists()).toBe(false)
+
+    await wrapper.setProps({ value: 'props.value is present now' })
+    wrapper.vm.$nextTick()
+
+    const clearElement2 = wrapper.find('.at-search-bar .at-search-bar__clear')
+    expect(clearElement2.exists()).toBe(true)
+
+    clearElement2.trigger('touchstart')
+    expect(onClear).toBeCalled()
   })
 })
