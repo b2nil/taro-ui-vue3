10233dd21ab6d75e21522ba44b170dca25c8a2de
Ken Ho <kentsu.ho@gmail.com>
2020-12-14 18:02:35 +0800

    fix(tests): update rate test cases

--- src/components/rate/__tests__/rate.spec.ts
+++ src/components/rate/__tests__/rate.spec.ts
@@ -1,4 +1,5 @@
 import { mount } from '@vue/test-utils'
+import { ref } from '@vue/runtime-core'
 import AtRate from '../index'
 
 const factory = (values = {}, slots = { default: [] }) => {
@@ -10,7 +11,7 @@ const factory = (values = {}, slots = { default: [] }) => {
 
 describe('AtRate Snap', () => {
   it('should render initial AtRate', () => {
-    const wrapper = factory({ isTest: true })
+    const wrapper = factory()
     expect(wrapper.element).toMatchSnapshot()
   })
 
@@ -27,29 +28,86 @@ describe('AtRate Snap', () => {
   it('should render AtRate -- props size', () => {
     const wrapper = factory({ size: '10' })
     expect(wrapper.element).toMatchSnapshot()
+
+    wrapper.findAll(".at-icon").forEach(el => {
+      expect(el.attributes('style')).toContain('font-size: 10px;')
+    })
   })
 
   it('should render AtRate -- props value', () => {
     const wrapper = factory({ value: 2 })
     expect(wrapper.element).toMatchSnapshot()
+    expect(wrapper.findAll('.at-rate__icon--on').length).toEqual(2)
   })
 
   it('should render AtRate -- props max', () => {
     const wrapper = factory({ max: 10 })
     expect(wrapper.element).toMatchSnapshot()
+    expect(wrapper.findAll('.at-rate__icon').length).toEqual(10)
   })
 
   it('should render AtRate -- props margin', () => {
     const wrapper = factory({ margin: 10 })
+
+    const pxTransform = function (size, designWidth) {
+      if (designWidth == null) {
+        throw new Error('pxTransform 函数在 H5 中运行需要把配置中的 `designWidth` 作为第二个参数传入')
+      }
+      return Math.ceil((((parseInt(size, 10) / 40) * 640) / designWidth) * 10000) / 10000 + 'rem'
+    }
+
+    const margin = `margin-right: ${pxTransform(10, 750)};`
+
     expect(wrapper.element).toMatchSnapshot()
+    wrapper.findAll(".at-rate__icon").forEach(el => {
+      expect(el.attributes('style')).toContain(margin)
+    })
+  })
+
+  it('should render AtRate -- props icon: star', () => {
+    const wrapper = factory()
+    expect(wrapper.element).toMatchSnapshot()
+    expect(wrapper.find(".at-icon-star-2").exists()).toBeTruthy()
+  })
+
+  it('should render AtRate -- props icon: heart', () => {
+    const wrapper = factory({ icon: 'heart' })
+    expect(wrapper.element).toMatchSnapshot()
+    expect(wrapper.find(".at-icon-heart-2").exists()).toBeTruthy()
+  })
+
+  it('should render AtRate -- props color', () => {
+    const wrapper = factory({ value: 1, color: 'teal' })
+    expect(wrapper.element).toMatchSnapshot()
+    expect(wrapper.find(".at-rate__icon--on .at-icon").attributes().style).toContain('color: teal')
   })
 })
 
 describe('AtRate Event', () => {
-  it('AtRate onChange Event', () => {
+  it('should trigger onChange Event', () => {
     const onChange = jest.fn()
     const wrapper = factory({ value: 2, onChange: onChange })
     wrapper.find('.at-rate__icon').trigger('tap')
     expect(onChange).toBeCalled()
+  })
+
+  it('should trigger "onUpdate:value" when vModel is used', async () => {
+    const modelValue = ref(1)
+    const wrapper = factory({
+      value: modelValue.value,
+      'onUpdate:value': jest.fn().mockImplementation((e) => {
+        modelValue.value = e
+      })
+    })
+    expect(wrapper.findAll('.at-rate__icon--on').length).toEqual(1)
+    wrapper.findAll('.at-rate__icon').forEach((el, i) => {
+      if (i === 2) {
+        el.trigger('tap')
+      }
+    })
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.value).toBe(3)
+      expect(wrapper.findAll('.at-rate__icon--on').length).toBe(3)
+    })
   })
 })
