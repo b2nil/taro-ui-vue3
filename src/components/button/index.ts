eca13817ddfc4b09f252fe6ff2c1da245dccf815
Ken Ho <kentsu.ho@gmail.com>
2021-03-20 10:11:13 +0800

    chore(button): warn changes of getUserProfile api

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -114,6 +114,13 @@ const AtButton = defineComponent({
     }
 
     function handleGetUserInfo(event) {
+      warn(
+        "2021 年 4 月 13 日后发布的新版本小程序，",
+        "开发者调用 `wx.getUserInfo` 或 `<button open-type=\"getUserInfo\"/>` 将不再弹出弹窗，",
+        "直接返回匿名的用户个人信息。",
+        "详情见：https://developers.weixin.qq.com/community/develop/doc/000cacfa20ce88df04cb468bc52801?idescene=6&page=10,",
+        "请使用 `getUserProfile` 进行适配。"
+      )
       props.onGetUserInfo?.(event)
     }
 
