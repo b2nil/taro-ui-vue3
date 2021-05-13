1b4c5a18144a3faf65736dced69bbd6f835959df
Ken Ho <kentsu.ho@gmail.com>
2021-05-13 13:50:11 +0800

    Deploy

---
+++ assets/index.md.2f30d764.lean.js
@@ -0,0 +1,1 @@
+import{o as t,c as e}from"./app.ec6bce23.js";const r='{"title":"","description":"","frontmatter":{},"relativePath":"index.md","lastUpdated":1619356091273}',a={};a.render=function(r,a,o,d,i,n){return t(),e("div")};export default a;export{r as __pageData};
