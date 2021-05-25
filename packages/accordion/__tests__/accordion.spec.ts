cddae6655b2ca7886312ff82fe42f5ee6ef22997
Ken Ho <kentsu.ho@gmail.com>
2021-05-25 15:25:53 +0800

    refactor(accordion): use sfc

---
+++ packages/accordion/__tests__/accordion.spec.ts
@@ -0,0 +1,254 @@
+import { ref } from 'vue'
+import AtAccordion from '../index.vue'
+import { AtIconBaseProps } from '@taro-ui-vue3/types/base'
+import { mountFactory, Slots } from '@taro-ui-vue3/test-utils/helper'
+import * as utils from '@taro-ui-vue3/utils/common'
+
+const factory = (
+  props = {},
+  slots: Slots = { default: ['按钮'] }
+) => {
+  return mountFactory(AtAccordion, {}, props, slots)
+}
+
+describe('AtAccordion', () => {
+  it('should render default AtAccordion', () => {
+    const wrapper = factory()
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('should render prop class', () => {
+    const wrapper = factory({ class: 'test' })
+    expect(
+      wrapper
+        .get('.at-accordion')
+        .classes()
+    ).toContain('test')
+  })
+
+  it('should render prop style', () => {
+    const wrapper = factory({ style: { color: 'red' } })
+    expect(
+      wrapper
+        .get('.at-accordion')
+        .attributes('style')
+    ).toEqual('color: red;')
+  })
+
+  it.each([
+    'title',
+    'note'
+  ])('should render prop %s', (propName) => {
+    const wrapper = factory({
+      [propName]: propName,
+    })
+    expect(
+      wrapper
+        .get(`.at-accordion__info__${propName}`)
+        .text()
+    ).toEqual(propName)
+    expect(
+      wrapper
+        .get('.at-accordion__info')
+        .element
+    ).toMatchSnapshot()
+  })
+
+  it.each`
+    open
+    ${true}
+    ${false}
+  `('should render prop open=$open', ({ open }) => {
+    const wrapper = factory({
+      open,
+    })
+
+    expect(
+      wrapper
+        .find(".at-accordion__arrow--folded")
+        .exists()
+    ).toBe(!!open)
+
+    expect(
+      wrapper
+        .find(".at-accordion__content--inactive")
+        .exists()
+    ).toBe(!open)
+  })
+
+  it.each([
+    ['', { value: 'chevron-down' }],
+    ['with color', { value: 'chevron-down', color: 'red' }],
+    ['with size', { value: 'chevron-down', size: 10 }],
+    ['with prefixClass', { value: 'star', prefixClass: 'prefix-class' }],
+  ])('should render prop icon %s', (desc, icon: AtIconBaseProps) => {
+    const wrapper = factory({
+      icon,
+    })
+
+    const iconClass = Boolean(icon.prefixClass)
+      ? `.${icon.prefixClass}-${icon.value}`
+      : `.at-icon-${icon.value}`
+
+    const iconEl = wrapper.find(iconClass)
+
+    expect(iconEl.exists()).toBeTruthy()
+    expect(iconEl.element).toMatchSnapshot()
+
+    if (icon.color) {
+      expect(
+        iconEl.attributes('style')
+      ).toEqual(`color: ${icon.color};`)
+    }
+
+    if (icon.size) {
+      expect(
+        iconEl.attributes('style')
+      ).toEqual(`font-size: ${icon.size}px;`)
+    }
+
+    if (icon.prefixClass) {
+      expect(iconEl.classes()).toContain(icon.prefixClass)
+      expect(iconEl.classes()).toContain(`${icon.prefixClass}-${icon.value}`)
+    }
+  })
+
+  it('should render prop hasBorder', async () => {
+    const wrapper = factory()
+
+    expect(
+      wrapper
+        .find(".at-accordion__header--noborder")
+        .exists()
+    ).toBeFalsy()
+
+    await wrapper.setProps({ hasBorder: false })
+    expect(
+      wrapper
+        .find(".at-accordion__header--noborder")
+        .exists()
+    ).toBeTruthy()
+  })
+})
+
+describe('AtAccordion Behavior', () => {
+  let delayQuerySelector: jest.SpyInstance
+
+  beforeEach(() => {
+    jest.mock('@taro-ui-vue3/utils/common')
+    jest.useFakeTimers()
+
+    delayQuerySelector = jest
+      .spyOn(utils, 'delayQuerySelector')
+      .mockImplementation((_, selectorStr, delayTime) => {
+        return new Promise(resolve => {
+          resolve([{ height: 30 }])
+        })
+      })
+  })
+
+  afterEach(() => {
+    delayQuerySelector.mockClear()
+  })
+
+  it('should emit click event', async () => {
+    const wrapper = factory()
+    await wrapper.find('.at-accordion__header').trigger('tap')
+    expect(wrapper.emitted()).toHaveProperty('click')
+    expect(wrapper.emitted('click')![0]).toEqual([true])
+  })
+
+  it('should not emit click event during animated toggle', async () => {
+    const onClick = jest.fn()
+    const wrapper = factory({
+      onClick
+    })
+
+    await wrapper.setProps({ open: true })
+
+    jest.advanceTimersByTime(400)
+    await wrapper.find('.at-accordion__header').trigger('tap')
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.emitted()).not.toHaveProperty('click')
+    expect(onClick).not.toBeCalled()
+  })
+
+  it('should toggle on accordion with animation', async () => {
+    const open = ref(false)
+    const onClick = jest.fn((e) => { open.value = e })
+    const wrapper = factory({
+      open: open.value,
+      onClick
+    })
+
+    let contentWapper = wrapper.get('.at-accordion__content')
+    expect(contentWapper.attributes('style')).toBeFalsy()
+    expect(contentWapper.classes()).toContain('at-accordion__content--inactive')
+
+    await wrapper.find('.at-accordion__header').trigger('tap')
+    await wrapper.setProps({ open: open.value })
+
+    jest.advanceTimersByTime(90)
+    await wrapper.vm.$nextTick()
+    contentWapper = wrapper.get('.at-accordion__content')
+    expect(contentWapper.attributes('style')).toEqual('height: 0px;')
+
+    jest.advanceTimersByTime(10)
+    await wrapper.vm.$nextTick()
+    contentWapper = wrapper.get('.at-accordion__content')
+    expect(contentWapper.attributes('style')).toEqual('height: 30px;')
+
+    jest.advanceTimersByTime(700)
+    await wrapper.vm.$nextTick()
+    contentWapper = wrapper.get('.at-accordion__content')
+    expect(contentWapper.attributes('style')).toEqual('')
+    expect(contentWapper.classes()).not.toContain('at-accordion__content--inactive')
+  })
+
+  it('should toggle off accordion with animation', async () => {
+    const open = ref(true)
+    const onClick = jest.fn((e) => { open.value = e })
+    const wrapper = factory({
+      open: open.value,
+      onClick
+    })
+
+    let contentWapper = wrapper.get('.at-accordion__content')
+    expect(contentWapper.classes()).not.toContain('at-accordion__content--inactive')
+
+    await wrapper.find('.at-accordion__header').trigger('tap')
+    await wrapper.setProps({ open: open.value })
+
+    jest.advanceTimersByTime(90)
+    await wrapper.vm.$nextTick()
+
+    contentWapper = wrapper.get('.at-accordion__content')
+    expect(contentWapper.attributes('style')).toEqual('height: 30px;')
+
+    jest.advanceTimersByTime(10)
+    await wrapper.vm.$nextTick()
+
+    await wrapper.find('.at-accordion__header').trigger('tap')
+    expect(onClick).toBeCalledTimes(1)
+
+    contentWapper = wrapper.get('.at-accordion__content')
+    expect(contentWapper.attributes('style')).toEqual('height: 0px;')
+
+    jest.advanceTimersByTime(700)
+    await wrapper.vm.$nextTick()
+
+    contentWapper = wrapper.get('.at-accordion__content')
+    expect(contentWapper.attributes('style')).toEqual('')
+    expect(contentWapper.classes()).toContain('at-accordion__content--inactive')
+  })
+
+  it('should not toggle with animation if animation is not used', async () => {
+    const wrapper = factory({
+      isAnimation: false
+    })
+    await wrapper.setProps({ open: true })
+    expect(delayQuerySelector).not.toBeCalled()
+  })
+
+})
---
+++ packages/accordion/index.vue
@@ -0,0 +1,190 @@
+<template>
+  <view
+    v-bind="$attrs"
+    class="at-accordion"
+  >
+    <!-- header -->
+    <view
+      :class="['at-accordion__header', {
+        'at-accordion__header--noborder': !hasBorder
+      }]"
+      @tap="handleClick"
+    >
+      <!-- icon -->
+      <text
+        v-if="Boolean(icon && icon.value)"
+        :class="['at-accordion__icon', iconClasses]"
+        :style="iconStyle"
+      />
+
+      <!-- info -->
+      <view class="at-accordion__info">
+        <view class="at-accordion__info__title">{{ title }}</view>
+        <view class="at-accordion__info__note">{{ note }}</view>
+      </view>
+
+      <!-- arrow -->
+      <view :class="['at-accordion__arrow', {
+        'at-accordion__arrow--folded': !!open
+      }]">
+        <text class="at-icon at-icon-chevron-down" />
+      </view>
+    </view>
+
+    <!-- content -->
+    <view
+      :class="[
+        'at-accordion__content',
+        {
+          'at-accordion__content--inactive': (!open && isCompleted) || startOpen
+        }
+      ]"
+      :style="contentStyle"
+    >
+      <view
+        :id="contentID"
+        class="at-accordion__body"
+      >
+        <slot />
+      </view>
+    </view>
+  </view>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  reactive,
+  ref,
+  watch,
+  computed,
+  toRefs,
+  PropType,
+  warn
+} from "vue"
+import {
+  AtAccordionProps,
+  AtAccordionState,
+} from "@taro-ui-vue3/types/accordion"
+import {
+  delayQuerySelector,
+  uuid
+} from "@taro-ui-vue3/utils/common"
+import {
+  useIconClasses,
+  useIconStyle
+} from "@taro-ui-vue3/composables/icon"
+
+const AtAccordion = defineComponent({
+  name: "AtAccordion",
+
+  emits: {
+    click: (open: boolean) => {
+      if (typeof open === 'boolean') {
+        return true
+      } else {
+        warn('click payload should be type boolean')
+        return false
+      }
+    }
+  },
+
+  props: {
+    open: Boolean,
+    title: {
+      type: String,
+      default: "",
+    },
+    icon: {
+      type: Object as PropType<AtAccordionProps["icon"]>,
+      default: () => ({ value: "" }),
+    },
+    hasBorder: {
+      type: Boolean,
+      default: true,
+    },
+    isAnimation: {
+      type: Boolean,
+      default: true,
+    },
+    note: {
+      type: String,
+      default: "",
+    }
+  },
+
+  setup(props: AtAccordionProps, { emit }) {
+    const startOpen = ref(false)
+    const isCompleted = ref(true)
+    const contentID = ref("content")
+    const state = reactive<AtAccordionState>({
+      wrapperHeight: 'unset'
+    })
+
+    const { iconStyle } = useIconStyle(props.icon)
+    const { iconClasses } = useIconClasses(props.icon, true)
+
+    const contentStyle = computed(() => ({
+      height: isCompleted.value
+        ? ''
+        : state.wrapperHeight === 'unset'
+          ? state.wrapperHeight
+          : `${state.wrapperHeight}px`,
+    }))
+
+    // watcher
+    watch(() => props.open, (open) => {
+      startOpen.value = !!open && !!props.isAnimation
+      toggleWithAnimation()
+    })
+
+    // methods
+    function handleClick() {
+      if (!isCompleted.value) return
+
+      contentID.value = "content_" + uuid()
+      emit("click", !props.open)
+    }
+
+    function toggleWithAnimation() {
+      if (!isCompleted.value || !props.isAnimation) return
+
+      isCompleted.value = false
+      delayQuerySelector(
+        this,
+        `#${contentID.value}.at-accordion__body`,
+        30
+      ).then((rect) => {
+        // @ts-ignore
+        const height = parseInt(rect[0].height.toString())
+        const startHeight = props.open ? 0 : height
+        const endHeight = props.open ? height : 0
+
+        startOpen.value = false
+        state.wrapperHeight = startHeight
+
+        setTimeout(() => {
+          state.wrapperHeight = endHeight
+        }, 100)
+
+        setTimeout(() => {
+          isCompleted.value = true
+        }, 700)
+      })
+    }
+
+    return {
+      ...toRefs(props),
+      contentID,
+      startOpen,
+      isCompleted,
+      iconClasses,
+      iconStyle,
+      contentStyle,
+      handleClick,
+    }
+  }
+})
+
+export default AtAccordion
+</script>
