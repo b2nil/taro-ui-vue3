e58914f248dccd6576b0acca4751af172746470b
Ken Ho <kentsu.ho@gmail.com>
2021-06-01 17:56:00 +0800

    refactor(avatar): use sfc

---
+++ packages/avatar/__tests__/avatar.spec.ts
@@ -0,0 +1,92 @@
+import { genMountFn } from '@taro-ui-vue3/test-utils/helper'
+import AtAvatar from '../index.vue'
+
+const mountFn = genMountFn(AtAvatar)
+
+describe('Avatar', () => {
+  it("should render default and match snapshot", () => {
+    const wrapper = mountFn()
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it.concurrent.each([
+    'large',
+    'normal',
+    'small'
+  ])('should render props size -- %s', async (size) => {
+    const wrapper = mountFn({ size })
+    expect(
+      wrapper
+        .get('.at-avatar')
+        .classes()
+    ).toContain(`at-avatar--${size}`)
+  })
+
+  it('should render normal size if prop size failed validation', async () => {
+    const wrapper = mountFn({ size: 'xsmall' })
+
+    expect(
+      '[Vue warn]: Invalid prop: custom validator check failed for prop \"size\".'
+    ).toHaveBeenWarned()
+
+    expect(
+      "[Vue warn]: Prop `size` must be one of <'large' | 'normal' | 'small'>"
+    ).toHaveBeenWarned()
+
+    expect(wrapper.find('.at-avatar--xsamll').exists()).toBe(false)
+    expect(wrapper.find('.at-avatar--normal').exists()).toBe(true)
+  })
+
+
+  it.concurrent('should render props circle', async () => {
+    const wrapper = mountFn({ circle: true })
+    expect(
+      wrapper
+        .get('.at-avatar')
+        .classes()
+    ).toContain(`at-avatar--circle`)
+  })
+
+  it('should render empty letter if prop text is not given', async () => {
+    const wrapper = mountFn()
+    const textEL = wrapper.find(`.at-avatar__text`)
+    expect(textEL.exists()).toBeTruthy()
+    expect(textEL.text()).toEqual("")
+  })
+
+  it.concurrent.each([
+    'image',
+    'text'
+  ])('should render prop %s', async (propName) => {
+    const wrapper = mountFn({
+      [propName]: 'https://test.com/img/100'
+    })
+    const isText = propName === 'text'
+    const imageEL = wrapper
+      .find(`.at-avatar__${isText ? propName : 'img'}`)
+
+    expect(imageEL.exists()).toBeTruthy()
+    if (isText) {
+      expect(imageEL.text()).toEqual('h')
+    }
+  })
+
+  it.concurrent('should render prop openData', async () => {
+    process.env.TARO_ENV = "weapp"
+    const wrapper = mountFn({ openData: { type: 'userAvatarUrl' } })
+    expect(
+      wrapper.find('open-data').exists()
+    ).toBeTruthy()
+    expect(
+      wrapper.find('open-data').attributes('type')
+    ).toBe('userAvatarUrl')
+    process.env.TARO_ENV = "h5"
+  })
+
+  it.concurrent('should not render open-data element if type of openData is not userAvatarUrl', async () => {
+    process.env.TARO_ENV = "weapp"
+    const wrapper = mountFn({ openData: { type: 'nickName' } })
+    expect(wrapper.find('open-data').exists()).toBe(false)
+    process.env.TARO_ENV = "h5"
+  })
+})
---
+++ packages/avatar/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <view
+    v-bind="$attrs"
+    :class="['at-avatar', {
+			'at-avatar--circle': circle,
+			[`at-avatar--${iconSize}`]: iconSize,
+    }]"
+  >
+    <open-data
+      v-if="isWEAPP && openData && openData.type === 'userAvatarUrl'"
+      :type="openData.type"
+    />
+    <image
+      v-else-if="image"
+      class="at-avatar__img"
+      :src="image"
+    />
+    <text
+      v-else
+      class="at-avatar__text"
+    >{{ letter }}</text>
+  </view>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, toRefs, PropType, warn } from 'vue'
+import { AtAvatarProps } from "@taro-ui-vue3/types/avatar"
+import { getEnvs } from "@taro-ui-vue3/utils"
+
+const SIZE_CLASS = {
+  large: 'large',
+  normal: 'normal',
+  small: 'small'
+}
+
+export default defineComponent({
+  name: "AtAvatar",
+
+  props: {
+    size: {
+      type: String as PropType<'large' | 'normal' | 'small'>,
+      default: 'normal',
+      validator: (prop: string) => ['large', 'normal', 'small'].includes(prop)
+    },
+    circle: Boolean,
+    text: String,
+    image: String,
+    openData: Object as PropType<{ type: 'userAvatarUrl' }>
+  },
+
+  setup(props: AtAvatarProps) {
+    const { isWEAPP } = getEnvs()
+
+    const letter = computed(() => {
+      return Boolean(props.text) ? String(props.text)[0] : ''
+    })
+
+    const iconSize = computed(() => {
+      let size = SIZE_CLASS[props.size!]
+
+      if (!size) {
+        warn(
+          "Prop `size` must be one of <'large' | 'normal' | 'small'>", "\nActual: '", props.size, "', now use 'normal' instead!"
+        )
+        size = 'normal'
+      }
+
+      return size
+    })
+
+
+    return {
+      ...toRefs(props),
+      isWEAPP,
+      letter,
+      iconSize
+    }
+  }
+})
+</script>
