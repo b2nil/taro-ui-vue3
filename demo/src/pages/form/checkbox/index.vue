e0f036d4c09f71bbb5edf990b0804248bc979d8f
Ken Ho <kentsu.ho@gmail.com>
2021-06-11 11:35:00 +0800

    demo(checkbox): use v-model instead onChange

--- demo/src/pages/form/checkbox/index.vue
+++ demo/src/pages/form/checkbox/index.vue
@@ -8,8 +8,7 @@
         <view class="checkbox-container">
           <at-checkbox
             :options="checkboxOption1"
-            :selected-list="checkedList1"
-            @change="handleChange"
+            v-model:selected-list="checkedList1"
           />
         </view>
       </example-item>
@@ -23,8 +22,7 @@
         <view class="checkbox-container">
           <at-checkbox
             :options="checkboxOption2"
-            :selected-list="checkedList2"
-            @change="handleChange2nd"
+            v-model:selected-list="checkedList2"
           />
         </view>
       </example-item>
@@ -38,22 +36,7 @@
         <view class="checkbox-container">
           <at-checkbox
             :options="checkboxOption3"
-            :selected-list="checkedList3"
-            @change="handleChange3rd"
-          />
-        </view>
-      </example-item>
-    </panel>
-
-    <panel
-      title="使用 vModel：`v-model:selected-list`"
-      no-padding
-    >
-      <example-item>
-        <view class="checkbox-container">
-          <at-checkbox
-            :options="checkboxOption4"
-            v-model:selectedList="checkedList4"
+            v-model:selected-list="checkedList3"
           />
         </view>
       </example-item>
@@ -70,11 +53,9 @@ interface IndexState {
   checkedList1: string[]
   checkedList2: string[]
   checkedList3: string[]
-  checkedList4: string[]
   checkboxOption1: CheckboxOption<string>[]
   checkboxOption2: CheckboxOption<string>[]
   checkboxOption3: CheckboxOption<string>[]
-  checkboxOption4: CheckboxOption<string>[]
 }
 
 export default defineComponent({
@@ -86,7 +67,6 @@ export default defineComponent({
       checkedList1: ['list1'],
       checkedList2: ['list1'],
       checkedList3: ['list1', 'list4'],
-      checkedList4: ['list2'],
       checkboxOption1: [
         { value: 'list1', label: 'iPhone X' },
         { value: 'list2', label: 'HUAWEI P20' },
@@ -134,47 +114,11 @@ export default defineComponent({
             '部分地区提供电子普通发票，用户可自行打印，效力等同纸质普通发票，具体以实际出具的发票类型为准。',
           disabled: true
         }
-      ],
-      checkboxOption4: [
-        {
-          value: 'list1',
-          label: 'iPhone X',
-          desc:
-            '部分地区提供电子普通发票，用户可自行打印，效力等同纸质普通发票，具体以实际出具的发票类型为准。'
-        },
-        { value: 'list2', label: 'HUAWEI P20' },
-        {
-          value: 'list3',
-          label: 'OPPO Find X',
-          desc:
-            '部分地区提供电子普通发票，用户可自行打印，效力等同纸质普通发票，具体以实际出具的发票类型为准。',
-        },
-        {
-          value: 'list4',
-          label: 'vivo NEX',
-          desc:
-            '部分地区提供电子普通发票，用户可自行打印，效力等同纸质普通发票，具体以实际出具的发票类型为准。',
-        }
       ]
     })
 
-    function handleChange(value: string[]): void {
-      state.checkedList1 = value
-    }
-
-    function handleChange2nd(value: string[]): void {
-      state.checkedList2 = value
-    }
-
-    function handleChange3rd(value: string[]): void {
-      state.checkedList3 = value
-    }
-
     return {
-      ...toRefs(state),
-      handleChange,
-      handleChange2nd,
-      handleChange3rd
+      ...toRefs(state)
     }
   }
 })
