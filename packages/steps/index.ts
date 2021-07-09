133a0717c9a1494ad5da7c3ea2865f3fceb13379
Ken Ho <kentsu.ho@gmail.com>
2021-07-09 12:46:14 +0800

    chore(steps):  use icon composables

--- packages/steps/index.ts
+++ packages/steps/index.ts
@@ -1,7 +1,9 @@
 import { h, defineComponent, computed, mergeProps, PropType } from 'vue'
 import { Text, View } from '@tarojs/components'
 import { CommonEvent } from '@tarojs/components/types/common'
-import { AtStepsProps } from '@taro-ui-vue3/types/steps'
+import { AtIconBaseProps } from '@taro-ui-vue3/types/base'
+import { AtStepsProps, Item } from '@taro-ui-vue3/types/steps'
+import { useIconStyle } from "@taro-ui-vue3/composables/icon"
 
 const AtSteps = defineComponent({
   name: "AtSteps",
@@ -23,13 +25,13 @@ const AtSteps = defineComponent({
 
   setup(props: AtStepsProps, { attrs, slots }) {
 
-    const stepItemClass = computed(() => (i) => ({
+    const genStepItemClasses = computed(() => (i: number) => ({
       'at-steps__item': true,
       'at-steps__item--active': i === props.current,
       'at-steps__item--inactive': i !== props.current
     }))
 
-    const itemStatusClass = computed(() => (item) => ({
+    const genItemStatusClasses = computed(() => (item: Item) => ({
       'at-icon': true,
       'at-steps__single-icon': true,
       'at-icon-check-circle': item.status === 'success',
@@ -39,11 +41,26 @@ const AtSteps = defineComponent({
       'at-steps__single-icon--error': item.status === 'error'
     }))
 
-    const itemIconClass = computed(() => (item) => ({
+    const genItemIconClasses = computed(() => (item: Item) => ({
       'at-icon': true,
-      [`at-icon-${item.icon.value}`]: item.icon.value,
+      [`at-icon-${item.icon?.value}`]: Boolean(item.icon?.value),
       'at-steps__circle-icon': true
     }))
+
+    const genItemIconStyle = (item: Item, i: number) => {
+      const iconInfo: AtIconBaseProps = {
+        value: item.icon!.value!,
+        size: item.icon!.size
+      }
+
+      iconInfo.color = props.current === i
+        ? item.icon!.activeColor
+        : item.icon!.inactiveColor
+
+      const { iconStyle } = useIconStyle(iconInfo)
+
+      return iconStyle.value
+    }
 
     function handleClick(current: number, event: CommonEvent): void {
       props.onChange(current, event)
@@ -56,7 +73,7 @@ const AtSteps = defineComponent({
         default: () => !!props.items && props.items.map((item, i) => (
           h(View, {
             key: item.title,
-            class: stepItemClass.value(i),
+            class: genStepItemClasses.value(i),
             onTap: handleClick.bind(this, i)
           }, {
             default: () => [
@@ -70,14 +87,19 @@ const AtSteps = defineComponent({
 
                   // status
                   item.status
-                    ? h(View, { class: itemStatusClass.value(item) })
+                    ? h(View, { class: genItemStatusClasses.value(item) })
                     : h(View, {
                       class: 'at-steps__circular'
                     }, {
                       default: () => [
                         item.icon
-                          ? h(Text, { class: itemIconClass.value(item) })
-                          : h(Text, { class: 'at-steps__num' }, { default: () => i + 1 })
+                          ? h(Text, {
+                            class: genItemIconClasses.value(item),
+                            style: genItemIconStyle(item, i)
+                          })
+                          : h(Text, {
+                            class: 'at-steps__num'
+                          }, { default: () => i + 1 })
                       ]
                     }),
 
