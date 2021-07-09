216f5d0dfd1c0187e9875e71bdb724632bfede37
Ken Ho <kentsu.ho@gmail.com>
2021-07-09 11:18:52 +0800

    feat(composables): add useIconClasses and useIconStyle

---
+++ packages/composables/icon.ts
@@ -0,0 +1,67 @@
+import { computed, CSSProperties } from 'vue'
+import { cssStringToObject } from '../utils/common'
+import type { AtIconBaseProps } from '../types/base'
+
+export function useIconClasses(
+  icon?: AtIconBaseProps,
+  allowPrefixClass?: boolean
+) {
+  const iconClasses = computed(() => {
+    if (allowPrefixClass) {
+      return {
+        [`${icon?.prefixClass || "at-icon"}`]: Boolean(icon),
+        [`${icon?.prefixClass || "at-icon"}-${icon?.value}`]: Boolean(
+          icon && icon.value
+        ),
+        [`${icon?.class}`]: Boolean(icon?.class)
+      }
+    }
+
+    return {
+      "at-icon": Boolean(icon),
+      [`at-icon-${icon?.value}`]: Boolean(icon && icon.value),
+      [`${icon?.class}`]: Boolean(icon?.class)
+    }
+  })
+
+  return {
+    iconClasses
+  }
+}
+
+export function useIconStyle(
+  icon?: AtIconBaseProps,
+  defaultColor: string = "",
+  defaultSize?: number,
+  pxTransform?: Function
+) {
+  const iconStyle = computed(() => {
+    let size: string
+
+    if (Boolean(icon && icon.size)) {
+      size = pxTransform
+        ? pxTransform(icon!.size)
+        : `${icon!.size}px`
+    } else {
+      size = Boolean(defaultSize) ? `${defaultSize}px` : ""
+    }
+
+    const style = Boolean(icon?.style)
+      ? typeof icon!.style === 'string'
+        ? cssStringToObject(icon!.style)
+        : icon!.style
+      : {}
+
+    return {
+      color: Boolean(icon && icon.color)
+        ? icon!.color
+        : defaultColor,
+      fontSize: size,
+      ...(style as CSSProperties)
+    }
+  })
+
+  return {
+    iconStyle
+  }
+}
